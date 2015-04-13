8e343f7671210fbebf7307a7f3b928359a341f54
Pierre-Marie de Rodat <derodat@adacore.com>
2015-04-13 17:43:26 +0200

    Python API: fix the gigantic memory leak for ASTNode wrappers
    ... and update the corresponding testcase to properly check the new
    ASTNode wrappers implementation.
    
    Followup on O323-002
    
    Change-Id: I94ff273f0738bd21f9d03fdd4837bbc66c0edb21

--- testsuite/tests/python/node_identity/test.py
+++ testsuite/tests/python/node_identity/test.py
@@ -3,31 +3,42 @@ Test that Python wrapper objects around C AST nodes are preserved across
 different wrappings.
 """
 
+import gc
+
 import libadalang
 
 
 ctx = libadalang.AnalysisContext()
 unit = ctx.create_from_file('foo.adb')
 
+# First check that user attributes on ASTNode are kept across wrappers.
+node = unit.root
+first_id = id(node)
+node.foobar = 1
 
-def pre_process(unit):
-    root = unit.root
-    root.foobar = 1
-    return id(root)
+node = None
+gc.collect()
+
+# At this point, the first "node" wrapper is supposed to be free'd: create
+# another one and check its attributes were preserved.
+node = unit.root
+last_id = id(node)
+assert first_id != last_id, (
+    'This testcase is meaningless if we get the same Python wrapper in the two'
+    ' testing points (and we did).'
+)
+
+assert node.foobar == 1, (
+    '"foobar" attribute was not preserved:'
+    ' got {} instead of 1'.format(node.foobar)
+)
 
 
-def post_process(unit):
-    root = unit.root
-    assert root.foobar == 1, (
-        'root.foobar = {} whereas 1 was expected instead'.format(
-            unit.foobar
-        )
-    )
-    return id(root)
+# Now make sure we have a working equality operator
+root1 = unit.root
+root2 = unit.root
+body = unit.root.body
+assert root1 == root2
+assert root1 != body
 
-
-first_id = pre_process(unit)
-last_id = post_process(unit)
-assert first_id == last_id, (
-    'Got different Python wrappers for the same C AST node')
 print 'Done.'

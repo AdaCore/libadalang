519ce0790b916a9767672f0ff28f3ab495528bfc
Pierre-Marie de Rodat <derodat@adacore.com>
2015-10-22 10:47:29 -0400

    Add comments to the extension system in Langkit_Support
    Change-Id: Ib97ff578d1af7f9a8b5675a6fa90274a8085e53b
    TN: OA04-004

--- langkit/langkit/support/langkit_support-extensions.ads
+++ langkit/langkit/support/langkit_support-extensions.ads
@@ -1,9 +1,26 @@
+--  This package helps to deal with AST_Node extensions
+
 package Langkit_Support.Extensions is
 
    type Extension_ID is new Natural;
+   --  Unique identifier for a class of extensions.
+   --
+   --  AST_Node can hold many extensions, so we need a way to perform lookups
+   --  on a specific class of extension (for instance: Python bindings
+   --  extensions). Having a unique identifier is the chosen way to achieve
+   --  this.
 
    function Register_Extension (Name : String) return Extension_ID;
+   --  Register an extension to the engine and return the corresponding ID.
+   --
+   --  Extension_ID is a scalar in order to make comparison as fast as
+   --  possible. On the other hand, it is more convenient for plug-ins to use
+   --  human-readable strings to identify extensions. So this function makes
+   --  the conversion between unique human-readable strings and unique IDs
+   --  (i.e. it returns always the same Extension_ID for the same string and
+   --  always returns different IDs for different strings).
 
    function Has_Extensions return Boolean;
+   --  Return whether at least one extension was registered.
 
 end Langkit_Support.Extensions;

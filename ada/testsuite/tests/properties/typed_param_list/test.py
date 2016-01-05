969ee0e8ae6447272fe226654de63ef10d3b61fb
Pierre-Marie de Rodat <derodat@adacore.com>
2016-01-05 12:48:54 +0100

    Add a testcase for SubprogramSpec.p_typed_param_list
    Change-Id: Icc6fec032458e9f5f6f207a361d7bb87f4fc0f04
    TN: OC22-013

---
+++ ada/testsuite/tests/properties/typed_param_list/test.py
@@ -0,0 +1,34 @@
+import libadalang
+
+
+source_template = '''
+procedure Foo {} is
+begin
+   null;
+end Foo;
+'''
+
+
+ctx = libadalang.AnalysisContext('utf-8')
+
+for i, params in enumerate([
+    '',
+    '(A : Integer)',
+    '(A, B : Integer; C : Boolean)',
+]):
+    if i > 0:
+        print('')
+
+    title = 'Params: {}'.format(params or '<empty>')
+    print(title)
+    print('=' * len(title))
+
+    unit = ctx.get_from_buffer('foo.adb', source_template.format(params))
+    subp_spec = unit.root.f_bodies[0].f_item.f_subp_spec
+    for i, (name, type_expr) in enumerate(subp_spec.p_typed_param_list):
+        assert isinstance(name, libadalang.Identifier)
+
+        if i > 0:
+            print('')
+        print('{}:'.format(name.f_tok.text))
+        type_expr.dump()

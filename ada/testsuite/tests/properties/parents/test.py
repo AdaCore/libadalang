8988f2ff903e6cae07d9f5498822a11c27202af5
Pierre-Marie de Rodat <derodat@adacore.com>
2016-01-12 10:02:01 +0100

    Add a testcase for the parents property
    Change-Id: I9c4a7e8393346270f7071993a855e5f4965c5872
    TN: P106-015

---
+++ ada/testsuite/tests/properties/parents/test.py
@@ -0,0 +1,21 @@
+import libadalang
+
+
+def short_repr(node):
+    return ('{}[{}]'.format(node.kind_name, node.sloc_range)
+            if node else
+            'None')
+
+
+ctx = libadalang.AnalysisContext('utf-8')
+unit = ctx.get_from_file('foo.adb')
+
+unit.root.dump()
+
+nodes = [unit.root] + list(
+    unit.root.findall(lambda n: isinstance(n, libadalang.SubprogramSpec))
+)
+for node in nodes:
+    print('Parents of {}:'.format(short_repr(node)))
+    for parent in node.parents:
+        print('  - {}'.format(short_repr(parent)))

7aa79b631c45cf60b0616d0a7fa8791cf7ae39f3
Raphael Amiard <amiard@adacore.com>
2016-02-04 11:57:38 +0100

    Pycharm checks: Don't run if pycharm is running.
    Change-Id: I2da6e1e6aba8c8727759ede200924e5afe96a6f0
    TN: OC09-040

--- ada/testsuite/tests/pycharmchecks/test.py
+++ ada/testsuite/tests/pycharmchecks/test.py
@@ -1,14 +1,17 @@
 from distutils.spawn import find_executable
 from os import path, environ
+from psutil import process_iter
 from subprocess import check_call
 import sys
 
 if find_executable('pycharm.sh'):
-    check_call([
-        sys.executable,
-        path.join(
-            environ['LIBADALANG_ROOTDIR'], "..",
-            "utils",
-            "print_pycharm_diagnostics.py"
-        )
-    ])
+    pycharm = [p for p in process_iter() if p.name().find("pycharm") != -1]
+    if not pycharm:
+        check_call([
+            sys.executable,
+            path.join(
+                environ['LIBADALANG_ROOTDIR'], "..",
+                "utils",
+                "print_pycharm_diagnostics.py"
+            )
+        ])

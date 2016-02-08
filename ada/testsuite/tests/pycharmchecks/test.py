9182cf5039dce6d6dedabc1606388c3501662797
Pierre-Marie de Rodat <derodat@adacore.com>
2016-02-08 10:19:59 +0100

    pycharmchecks: make testcase work even if psutils in not available
    Change-Id: I51dbff74af2bdca1bb3d3e555d6a705c851d1d29
    TN: OC09-040

--- ada/testsuite/tests/pycharmchecks/test.py
+++ ada/testsuite/tests/pycharmchecks/test.py
@@ -1,11 +1,16 @@
 from distutils.spawn import find_executable
 from os import path, environ
-from psutil import process_iter
 from subprocess import check_call
 import sys
 
-if find_executable('pycharm.sh'):
+try:
+    from psutil import process_iter
+except ImportError:
+    pycharm = []
+else:
     pycharm = [p for p in process_iter() if p.name().find("pycharm") != -1]
+
+if find_executable('pycharm.sh'):
     if not pycharm:
         check_call([
             sys.executable,

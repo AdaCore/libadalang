1a8a92a539693715f2931a24b839d5983447b614
Pierre-Marie de Rodat <derodat@adacore.com>
2016-03-01 15:20:58 +0100

    Testsuite: add a test driver to leverage ASTEval
    Change-Id: I5855f4d15e3cb1f8d17fbd06787e858ddb211b56
    TN: P212-008

--- ada/testsuite/testsuite_support/__init__.py
+++ ada/testsuite/testsuite_support/__init__.py
@@ -13,6 +13,7 @@ if not with_gnatpython:
 
 
 import testsuite_support.adaapi_driver
+import testsuite_support.asteval_driver
 import testsuite_support.capi_driver
 import testsuite_support.parser_driver
 import testsuite_support.python_driver
@@ -22,6 +23,7 @@ class Testsuite(BaseTestsuite):
     TEST_SUBDIR = 'tests'
     DRIVERS = {
         'parser': testsuite_support.parser_driver.ParserDriver,
+        'asteval': testsuite_support.asteval_driver.ASTEvalDriver,
         'ada-api': testsuite_support.adaapi_driver.CAPIDriver,
         'c-api': testsuite_support.capi_driver.CAPIDriver,
         'python': testsuite_support.python_driver.PythonDriver,

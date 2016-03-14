ee82b34a5dcc617f826e18ea684f15e89305d5ae
Pierre-Marie de Rodat <derodat@adacore.com>
2016-03-14 12:33:43 +0100

    Add a test for BaseId.parent_callexpr
    Change-Id: Icdf0f27ce81174f46a596525f87ed276b69b82b0
    TN: P308-003

---
+++ ada/testsuite/tests/symbol_resolution/parent_callexpr/foo.adb
@@ -0,0 +1,6 @@
+procedure Foo is
+begin
+   C (X, 1);
+   A.B.C (X, 1);
+   C (X) (13);
+end Foo;
---
+++ ada/testsuite/tests/symbol_resolution/parent_callexpr/test.py
@@ -0,0 +1,43 @@
+# Test the behavior of the p_parent_callexpr property on various children for
+# CallExpr nodes.
+
+import libadalang as lal
+
+import source
+
+
+with open('foo.adb') as f:
+    src_lines = f.readlines()
+src_slice = lambda node: source.src_slice(src_lines, node.sloc_range)
+
+
+ctx = lal.AnalysisContext()
+unit = ctx.get_from_file('foo.adb')
+
+
+calls = unit.root.find(lal.HandledStatements).f_statements
+simple_call, prefix_call, nested_call = calls
+
+for i, (call, name) in enumerate((
+    (simple_call, simple_call.f_name),
+    (simple_call, simple_call.f_suffix.f_params[0].f_expr),
+
+    (prefix_call, prefix_call.f_name.f_prefix.f_prefix),
+    (prefix_call, prefix_call.f_name.f_prefix.f_suffix),
+    (prefix_call, prefix_call.f_name.f_suffix),
+
+    (nested_call, nested_call.f_name.f_name),
+    (nested_call, nested_call.f_name.f_suffix.f_params[0].f_expr),
+)):
+    if i > 0:
+        print('')
+    print(src_slice(call))
+    column = name.sloc_range.start.column - call.sloc_range.start.column
+
+    p = name.p_parent_callexpr
+    p_text = (src_slice(p)
+              if isinstance(p, lal.CallExpr) else
+              str(p))
+    print('{}^ parent_callexpr = {}'.format(
+        ' ' * column, p_text
+    ))

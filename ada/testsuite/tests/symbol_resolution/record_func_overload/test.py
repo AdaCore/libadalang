9f3306c2a48b93cb688db1001feee38a5ddf5ae6
Pierre-Marie de Rodat <derodat@adacore.com>
2016-04-21 17:32:11 +0200

    Add a testcase for record field symbol resolution with overloaded funcs
    Change-Id: I3f2df1f00a532159df9c0490a5cd1b7c8c483866
    TN: P415-016

---
+++ ada/testsuite/tests/symbol_resolution/record_func_overload/test.py
@@ -0,0 +1,34 @@
+# Test the behavior of the p_entities property on various expressions involving
+# overloaded functions that return records and field accesses on the returned
+# records.
+
+import libadalang as lal
+
+import source
+
+
+with open('foo.adb') as f:
+    src_lines = f.readlines()
+src_slice = lambda node: source.src_slice(src_lines, node.sloc_range)
+
+
+ctx = lal.AnalysisContext()
+unit = ctx.get_from_file('foo.adb')
+unit.populate_lexical_env()
+
+
+# All pragmas contain call/prefix expressions used for the symbol resolution
+# test.
+for p in unit.root.finditer(lal.PragmaNode):
+    assert len(p.f_args) == 1
+    expr = p.f_args[0].f_expr
+    print('{} resolves to:'.format(src_slice(expr)))
+
+    entities = expr.p_entities
+
+    # Sort matches before printing them so that the output is guaranteed to
+    # be stable.
+    for e in sorted(entities, key=lambda n: n.sloc_range.start.line):
+        print('    {}'.format(src_slice(e)))
+    if not entities:
+        print('    <none>')

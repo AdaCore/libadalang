b3731a839f5512c508b4cea336d6e6e1ac3afa02
Pierre-Marie de Rodat <derodat@adacore.com>
2016-04-22 17:44:42 +0200

    Add a testcase for variant record fields symbol resolution
    Change-Id: I508b3145ac7f29d932e70aa253aded1b58a12fa8
    TN: P415-016

---
+++ ada/testsuite/tests/symbol_resolution/record_variants/foo.ads
@@ -0,0 +1,24 @@
+package Foo is
+
+   type Kind_Type is (Integer, Character, Boolean);
+
+   type Record_Type (K : Kind_Type) is record
+      case K is
+         when Integer =>
+            I : Integer;
+         when Character =>
+            C : Character;
+         when others =>
+            B : Boolean;
+      end case;
+   end record;
+
+   R : Record_Type := (K => Integer, I => 0);
+
+   pragma Test (R.K);
+   pragma Test (R.I);
+   pragma Test (R.C);
+   pragma Test (R.B);
+   pragma Test (R.Z);
+
+end Foo;

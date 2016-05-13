7b096547dc53f21d11a86806921037e8fc2f0871
Pierre-Marie de Rodat <derodat@adacore.com>
2016-05-13 12:24:27 +0200

    Testsuite: run the Ada symbol resolution driver
    ... and check that its output is consistent with the Python driver's.
    
    Change-Id: Ib56cbc3d2d8871f29459c3b58def96f98e9ee290
    TN: P509-043

--- ada/testsuite/testsuite_support/symbol_resolution_driver.py
+++ ada/testsuite/testsuite_support/symbol_resolution_driver.py
@@ -1,3 +1,4 @@
+import difflib
 import os
 import os.path
 
@@ -15,3 +16,29 @@ class SymbolResolutionDriver(PythonDriver):
                                     'symbol_resolution_driver.py')
         self.tear_up_helper()
         self.py_args = self.input_sources
+
+    @catch_test_errors
+    def run(self):
+        # Run both the Python and the Ada drivers for symbol resolution
+        py_output = self.run_and_check(
+            [self.python_interpreter, self.py_file] + self.py_args,
+            for_debug=True
+        )
+        ada_output = self.run_and_check(
+            ['symres'] + self.input_sources,
+            # TODO: enable memcheck once proper memory management in properties
+            # is implemented.
+            for_debug=True, memcheck=False, append_output=False,
+        )
+
+        diff = '\n'.join(difflib.unified_diff(py_output.splitlines(),
+                                              ada_output.splitlines(),
+                                              fromfile='python',
+                                              tofile='ada',
+                                              lineterm=''))
+        if diff:
+            with open(self.output_file, 'a') as f:
+                f.write('\n')
+                f.write('The Python driver and the Ada driver did not return'
+                        ' the same result:\n')
+                f.write(diff)

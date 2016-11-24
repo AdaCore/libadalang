2ba0f242712540fd4b0c36bc9b2bc33bd6b94c39
Pierre-Marie de Rodat <derodat@adacore.com>
2016-11-24 13:05:05 +0100

    Add a testcase for invalid unit names in Get_Unit_From_Provider
    Change-Id: I4750bed4971bb2bd5f072b9fb8856316a428c4ba
    TN: PA05-041

---
+++ ada/testsuite/tests/ada-api/project_ufp_invalid/main.adb
@@ -0,0 +1,67 @@
+with Ada.Exceptions;                  use Ada.Exceptions;
+with Ada.Strings.Wide_Wide_Unbounded; use Ada.Strings.Wide_Wide_Unbounded;
+with Ada.Text_IO;                     use Ada.Text_IO;
+
+with GNATCOLL.Projects; use GNATCOLL.Projects;
+with GNATCOLL.VFS;      use GNATCOLL.VFS;
+
+with Langkit_Support.Text; use Langkit_Support.Text;
+
+with Libadalang.Analysis;            use Libadalang.Analysis;
+with Libadalang.AST;                 use Libadalang.AST;
+with Libadalang.AST.Types;           use Libadalang.AST.Types;
+with Libadalang.Unit_Files;          use Libadalang.Unit_Files;
+with Libadalang.Unit_Files.Projects; use Libadalang.Unit_Files.Projects;
+
+procedure Main is
+
+   function Load_Project (File : String) return Project_Tree_Access;
+
+   function "+" (S : Wide_Wide_String) return Unbounded_Wide_Wide_String
+      renames To_Unbounded_Wide_Wide_String;
+   function "+" (S : Unbounded_Wide_Wide_String) return Wide_Wide_String
+      renames To_Wide_Wide_String;
+
+   ------------------
+   -- Load_Project --
+   ------------------
+
+   function Load_Project (File : String) return Project_Tree_Access is
+      Result : constant Project_Tree_Access := new Project_Tree;
+   begin
+      Load (Result.all, Create (+File));
+      return Result;
+   end Load_Project;
+
+   Prj  : Project_Tree_Access := Load_Project ("p.gpr");
+   UFP  : Project_Unit_File_Provider_Type := Create (Prj);
+   Ctx  : Analysis_Context :=
+      Create (Unit_File_Provider => UFP'Unrestricted_Access);
+   Unit : Analysis_Unit;
+
+   LF    : constant := Character'Pos (ASCII.LF);
+   Space : constant := Character'Pos (' ');
+
+   Filenames : array (Positive range <>) of Unbounded_Wide_Wide_String :=
+     (+(1 => Wide_Wide_Character'Val (LF)),
+      +"unknown_unit");
+
+begin
+   for File of Filenames loop
+      Put_Line ("Trying to get unit: " & Image (+File, With_Quotes => True));
+      begin
+         Unit := Get_From_Provider (Ctx, +File, Unit_Specification);
+
+         Put_Line ("...   got no exception. Unacceptable!");
+      exception
+         when Exc : Invalid_Unit_Name_Error =>
+            Put_Line ("   ... got an exception:");
+            Put_Line ("   " & Exception_Name (Exc));
+            Put_Line ("   " & Exception_Message (Exc));
+      end;
+   end loop;
+
+   Destroy (Ctx);
+   Free (Prj);
+   Put_Line ("Done.");
+end Main;

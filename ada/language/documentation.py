3adb029221cd123f2b969dcfd22f1bc74db3f605
Pierre-Marie de Rodat <derodat@adacore.com>
2016-11-25 18:47:04 +0100

    Add documentations for project unit file providers extensions
    Change-Id: I4443535a2c351fe1dc08668e9d4c091ce0afd1ab
    TN: PA05-041

--- ada/language/documentation.py
+++ ada/language/documentation.py
@@ -1,2 +1,24 @@
 libadalang_docs = {
+    'libadalang.project_scenario_variable': """
+        Couple name/value to define a scenario variable for a project.
+    """,
+    'libadalang.create_project_unit_file_provider': """
+        Load the project file at Project_File and return an unit file provider
+        that uses it.
+
+        % if lang == 'python':
+            If provided, Scenario_Vars must be a dict with key strings and key
+            values to describe the set of scenario variables for this project.
+        % else:
+            If not ${null}, Scenario_Vars must point to an array of
+            ${capi.get_name('project_scenario_variable')} couples to provide
+            scenario variables for this project. The last element of this array
+            must end with a { ${null}, ${null} } couple.
+        % endif
+
+        % if lang == 'c':
+            When done with it, the result must be free'd with
+            ${capi.get_name('destroy_unit_file_provider')}.
+        % endif
+    """,
 }

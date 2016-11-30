45ca77893f7d9671208702c452f93be4a3eeb37b
Pierre-Marie de Rodat <derodat@adacore.com>
2016-11-30 17:16:02 +0100

    Refactor the env_hook implementation
    This is to reduce code duplication.
    
    Change-Id: I46d42b25a5d82b8ba621ab51ce53b5195de600ed
    TN: PB23-020

--- ada/extensions/src/libadalang-unit_files-env_hook.adb
+++ ada/extensions/src/libadalang-unit_files-env_hook.adb
@@ -2,6 +2,19 @@ with Libadalang.AST.Types; use Libadalang.AST.Types;
 
 package body Libadalang.Unit_Files.Env_Hook is
 
+   function Get_Main_Env (Unit : Analysis_Unit) return Lexical_Env;
+   --  Return the main lexical environment for some analysis unit. For a
+   --  library-level package declaration for instance, this is the environment
+   --  of the package itself.
+
+   procedure Handle_Name
+     (Ctx      : Analysis_Context;
+      Name     : Ada_Node;
+      Main_Env : in out Lexical_Env);
+   --  Helper for the evironment hook. Fetch the unit for the spec file that
+   --  Name designates, populate its lexical environment and assign its main
+   --  environment to Main_Env.
+
    procedure Handle_With_Decl (Ctx : Analysis_Context; Names : Name_List);
    --  Helper for the environment hook to handle WithDecl nodes
 
@@ -9,12 +22,7 @@ package body Libadalang.Unit_Files.Env_Hook is
      (Ctx         : Analysis_Context;
       Node        : Body_Node;
       Initial_Env : in out Lexical_Env);
-   --  Helper for the environment hook to handle unit body nodes
-
-   function Get_Main_Env (Unit : Analysis_Unit) return Lexical_Env;
-   --  Return the main lexical environment for some analysis unit. For a
-   --  library-level package declaration for instance, this is the environment
-   --  of the package itself.
+   --  Helper for the environment hook to handle library-level unit body nodes
 
    --------------
    -- Env_Hook --
@@ -29,33 +37,47 @@ package body Libadalang.Unit_Files.Env_Hook is
    begin
       if Node.all in With_Clause_Type'Class then
          Handle_With_Decl (Ctx, With_Clause (Node).F_Packages);
-      elsif Node.all in Body_Node_Type'Class then
-         Handle_Unit_Body (Ctx, Body_Node (Node), Initial_Env);
+
+      elsif Node.Parent.all in Library_Item_Type'Class then
+         if Node.all in Body_Node_Type'Class then
+            Handle_Unit_Body (Ctx, Body_Node (Node), Initial_Env);
+         end if;
       end if;
    end Env_Hook;
+
+   -----------------
+   -- Handle_Name --
+   -----------------
+
+   procedure Handle_Name
+     (Ctx      : Analysis_Context;
+      Name     : Ada_Node;
+      Main_Env : in out Lexical_Env)
+   is
+      UFP      : constant Unit_File_Provider_Access_Cst :=
+         Unit_File_Provider (Ctx);
+      Name_Str : constant String := UFP.Get_File (Name, Unit_Specification);
+
+      --  TODO??? Find a proper way to handle file not found, parsing error,
+      --  etc.
+      Unit : Analysis_Unit := Get_From_File (Ctx, Name_Str);
+   begin
+      if Root (Unit) /= null then
+         Populate_Lexical_Env (Unit);
+         Main_Env := Get_Main_Env (Unit);
+         Reference_Unit (From => Get_Unit (Name), Referenced => Unit);
+      end if;
+   end Handle_Name;
 
    ----------------------
    -- Handle_With_Decl --
    ----------------------
 
    procedure Handle_With_Decl (Ctx : Analysis_Context; Names : Name_List) is
+      Dummy : Lexical_Env := AST_Envs.Empty_Env;
    begin
       for N of Names.all loop
-         declare
-            UFP : constant Unit_File_Provider_Access_Cst :=
-               Unit_File_Provider (Ctx);
-            Unit_File_Name : constant String :=
-               UFP.Get_File (N, Unit_Specification);
-            Unit           : constant Analysis_Unit :=
-               Get_From_File (Ctx, Unit_File_Name);
-         begin
-            --  TODO??? Should we do something special if the Unit has parsing
-            --  errors?
-            if Root (Unit) /= null then
-               Populate_Lexical_Env (Unit);
-               Reference_Unit (From => Get_Unit (Names), Referenced => Unit);
-            end if;
-         end;
+         Handle_Name (Ctx, N, Dummy);
       end loop;
    end Handle_With_Decl;
 
@@ -70,36 +92,22 @@ package body Libadalang.Unit_Files.Env_Hook is
    is
       Names     : Name_Array_Access;
    begin
-      --  If this unit is not a body we are interested in, there is no spec to
-      --  process.
-      if Node.all not in Subp_Body_Type'Class
-         and then Node.all not in Package_Body_Type'Class
+      --  If this not a library-level subprogram/package body, there is no spec
+      --  to process.
+      if Node.all not in Package_Body_Type'Class
+         and then Node.all not in Subp_Body_Type'Class
       then
          return;
       end if;
 
       Names := Node.P_Defining_Names;
+      pragma Assert (Names.N = 1);
 
       declare
-         UFP : constant Unit_File_Provider_Access_Cst :=
-            Unit_File_Provider (Ctx);
-         N    : constant Ada_Node := Ada_Node (Names.Items (1));
-         Name : constant String := UFP.Get_File (N, Unit_Specification);
-
-         --  TODO??? Maybe we should just return when the file does not exist:
-         --  after all it is legal.
-         Unit : Analysis_Unit := Get_From_File (Ctx, Name);
+         N : constant Ada_Node := Ada_Node (Names.Items (1));
       begin
          Dec_Ref (Names);
-         if Root (Unit) /= null then
-            Populate_Lexical_Env (Unit);
-            Initial_Env := Get_Main_Env (Unit);
-            Reference_Unit (From => Get_Unit (Node), Referenced => Unit);
-         end if;
-      exception
-         when Property_Error =>
-            Dec_Ref (Names);
-            raise;
+         Handle_Name (Ctx, N, Initial_Env);
       end;
    end Handle_Unit_Body;
 

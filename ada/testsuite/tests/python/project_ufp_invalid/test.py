6e9774443a15302f5cd79c1b0b76ddc5302b6a80
Pierre-Marie de Rodat <derodat@adacore.com>
2016-11-30 13:56:38 +0100

    Test invalid project loading in the Python API
    Change-Id: I6ea2971e76bf1b377b5ea17a9b65283246e2d0ef
    TN: PA05-041

--- ada/testsuite/tests/python/project_ufp_invalid/test.py
+++ ada/testsuite/tests/python/project_ufp_invalid/test.py
@@ -1,5 +1,14 @@
 import libadalang
 
+for project_file in ('invalid.gpr', 'does_not_exist.gpr'):
+    print('Trying to load invalid project: {}'.format(project_file))
+    try:
+        ufp = libadalang.UnitFileProvider.for_project('invalid.gpr')
+    except libadalang.InvalidProjectError as exc:
+        print('   ... got an exception: {}'.format(exc))
+    else:
+        print('   ... got no exception. Unacceptable!')
+
 ctx = libadalang.AnalysisContext(
     unit_file_provider=libadalang.UnitFileProvider.for_project('p.gpr')
 )

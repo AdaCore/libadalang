956197cec82459e912a782749877597ab64be046
Pierre-Marie de Rodat <derodat@adacore.com>
2016-11-30 17:18:38 +0100

    Add a testcase for auto-WITH of parent units specification
    Change-Id: I86a3d1c6c64452c8af9a0e5dab0b59b1d356ee92
    TN: PB23-020

---
+++ ada/testsuite/tests/symbol_resolution/cross_parent_unit/p1-child.ads
@@ -0,0 +1,5 @@
+package P1.Child is
+   type Record_Type is new P1.Record_Type;
+
+   pragma Test (P1.Record_Type);
+end P1.Child;
---
+++ ada/testsuite/tests/symbol_resolution/cross_parent_unit/p2.ads
@@ -0,0 +1,7 @@
+with P1.Child;
+
+package P2 is
+   R : P1.Child.Record_Type;
+
+   pragma Test (P1.Child.Record_Type);
+end P2;

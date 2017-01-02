19c91c047a19e58a25d42edb83f5a33e0546f0c0
Pierre-Marie de Rodat <derodat@adacore.com>
2017-01-02 16:58:50 +0100

    tests/python/project_ufp_invalid: avoid stdout/err interleaving
    Flush stdout after each message written so that messages in
    stdout/stderr are output in the expected order.
    
    Change-Id: I189affd05a8c17812476316a969a575622065d9a
    TN: PA05-041

--- ada/testsuite/tests/python/project_ufp_invalid/test.py
+++ ada/testsuite/tests/python/project_ufp_invalid/test.py
@@ -1,25 +1,32 @@
+import sys
+
 import libadalang
 
+
+def pflush(message):
+    sys.stdout.write(message + '\n')
+    sys.stdout.flush()
+
 for project_file in ('invalid.gpr', 'does_not_exist.gpr'):
-    print('Trying to load invalid project: {}'.format(project_file))
+    pflush('Trying to load invalid project: {}'.format(project_file))
     try:
         ufp = libadalang.UnitFileProvider.for_project('invalid.gpr')
     except libadalang.InvalidProjectError as exc:
-        print('   ... got an exception: {}'.format(exc))
+        pflush('   ... got an exception: {}'.format(exc))
     else:
-        print('   ... got no exception. Unacceptable!')
+        pflush('   ... got no exception. Unacceptable!')
 
 ctx = libadalang.AnalysisContext(
     unit_file_provider=libadalang.UnitFileProvider.for_project('p.gpr')
 )
 
 for filename in ('\n', ' '):
-    print('Trying to get unit: {}'.format(repr(filename)))
+    pflush('Trying to get unit: {}'.format(repr(filename)))
     try:
         unit = ctx.get_from_provider(filename, 'body')
     except libadalang.InvalidUnitNameError as exc:
-        print('   ... got an exception: {}'.format(exc))
+        pflush('   ... got an exception: {}'.format(exc))
     else:
-        print('   ... got no exception. Unacceptable!')
+        pflush('   ... got no exception. Unacceptable!')
 
 print "Done."

5acda22932ac10a4ca79f31e1cfa70c2dc7eb408
Pierre-Marie de Rodat <derodat@adacore.com>
2017-01-09 17:42:19 +0100

    Testsuite/ACATS: provide faulting source file in error message
    Change-Id: I9b4a07730f3e120096620aca434b7ef1e417741e
    TN: PC12-003

--- ada/testsuite/tests/acats_parse/test.py
+++ ada/testsuite/tests/acats_parse/test.py
@@ -1,10 +1,12 @@
 from glob import glob
 import os
-from os import path
+import traceback
 
 import libadalang as lal
 
-acats_dir = path.join(os.environ['LIBADALANG_ROOTDIR'], 'testsuite', 'acats')
+acats_dir = os.path.join(os.environ['LIBADALANG_ROOTDIR'],
+                         'testsuite',
+                         'acats')
 
 if not os.path.isdir(acats_dir):
     print "FAIL: You need to put the acats repo in ada/testsuite/acats"
@@ -12,16 +14,21 @@ else:
     c = lal.AnalysisContext()
 
     # Find every C test in the ACATS
-    for d in glob(path.join(acats_dir, 'acats-4', 'tests', 'c*')):
+    for d in glob(os.path.join(acats_dir, 'acats-4', 'tests', 'c*')):
         for root, dirs, files in os.walk(d):
             for filename in files:
+                filepath = os.path.join(root, filename)
                 if filename.endswith('.a') or filename.endswith('.ada'):
 
                     # Parse the file
-                    u = c.get_from_file(os.path.join(root, filename))
+                    try:
+                        u = c.get_from_file(filepath)
+                    except Exception:
+                        print "ERROR in {}:".format(filepath)
+                        raise
 
                     # Check that the parsing generated no diagnostics
                     if u.diagnostics:
-                        print "ERROR in {}:".format(filename)
+                        print "ERROR in {}:".format(filepath)
                         for d in u.diagnostics:
                             print d

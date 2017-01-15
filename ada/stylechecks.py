0b1104add66c7d5480b026f27ed7377c919ca6e8
Raphael Amiard <amiard@adacore.com>
2017-01-15 19:41:41 +0100

    Q114-003: move langkit stylechecks to langkit
    Change-Id: Ib3c667f8f5f5198f5de9f0022b62e3ada4d692df

--- ada/stylechecks.py
+++ ada/stylechecks.py
@@ -4,13 +4,11 @@ import os
 from os.path import join
 import sys
 
-ADA_DIR = join(os.path.dirname(os.path.abspath(__file__)))
+ADA_DIR = os.path.dirname(os.path.abspath(__file__))
 ROOT_DIR = join(ADA_DIR, '..')
 
-DIRS = ('ada', 'langkit', 'utils')
-EXCLUDES = ('tmp', 'doc', join('stylechecks', 'tests.py'),
-            join('langkit', 'adalog', 'obj'),
-            join('ada', 'testsuite', 'acats'))
+DIRS = ('ada', 'utils')
+EXCLUDES = ('tmp', 'doc', join('testsuite', 'acats'))
 
 sys.path.append(join(ROOT_DIR, "langkit"))
 

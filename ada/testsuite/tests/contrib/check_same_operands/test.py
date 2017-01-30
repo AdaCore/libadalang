2e1119a27a77276dcf8891f6af53a1cbc8148378
Pierre-Marie de Rodat <derodat@adacore.com>
2017-01-30 10:44:22 +0100

    Update contrib testcase after check_same_args renaming
    TN: minor

--- ada/testsuite/tests/contrib/check_same_operands/test.py
+++ ada/testsuite/tests/contrib/check_same_operands/test.py
@@ -6,6 +6,6 @@ sys.path.append(os.path.join(
     os.environ['LIBADALANG_ROOTDIR'],
     '..', 'contrib'
 ))
-import check_same_args
-check_same_args.main(check_same_args.parser.parse_args(
+import check_same_operands
+check_same_operands.main(check_same_operands.parser.parse_args(
     ['foo.adb']))

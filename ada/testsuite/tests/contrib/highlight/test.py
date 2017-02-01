ffaca8d0a1bc160bc1c4b7445061421cef8ebdf7
Pierre-Marie de Rodat <derodat@adacore.com>
2017-02-01 16:21:28 +0100

    Add a testcase for the highlighter example
    TN: Q131-031

---
+++ ada/testsuite/tests/contrib/highlight/test.py
@@ -0,0 +1,10 @@
+import subprocess
+
+from utils import gprbuild, in_contrib
+
+
+gprbuild(in_contrib('highlight', 'highlight.gpr'))
+subprocess.check_call(
+    [in_contrib('highlight', 'obj', 'highlight'), 'example.adb'],
+    cwd=in_contrib('highlight')
+)

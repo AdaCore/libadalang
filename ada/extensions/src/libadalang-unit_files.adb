a24ab8c04c577c16bea4ccc75c4f0b8c5a96f33c
Pierre-Marie de Rodat <derodat@adacore.com>
2017-05-10 12:18:28 +0200

    WITH clauses: make visible all mentionned units (not just the named one)
    TN: Q328-005

--- ada/extensions/src/libadalang-unit_files.adb
+++ ada/extensions/src/libadalang-unit_files.adb
@@ -9,18 +9,44 @@ package body Libadalang.Unit_Files is
       Name : Ada_Node;
       Kind : Unit_Kind) return Analysis_Unit
    is
-      UFP      : constant Unit_Provider_Access_Cst := Unit_Provider (Ctx);
+      UFP              : constant Unit_Provider_Access_Cst :=
+         Unit_Provider (Ctx);
+      Unit, First_Unit : Analysis_Unit;
+      Current_Name     : Ada_Node := Name;
 
-      --  TODO??? Find a proper way to handle file not found, parsing error,
-      --  etc.
-      Unit : Analysis_Unit := UFP.Get_Unit (Ctx, Name, Kind);
    begin
-      if Root (Unit) /= null then
-         Populate_Lexical_Env (Unit);
-         Reference_Unit (From => Get_Unit (Name), Referenced => Unit);
-      end if;
+      --  In Ada, "with A.B" gives visibility to A and A.B. To process all
+      --  "mentionned" units, the following loop iterates on ["A.B", "A"].
 
-      return Unit;
+      while Current_Name /= null loop
+         --  TODO??? Find a proper way to handle file not found, parsing error,
+         --  etc.
+         Unit := UFP.Get_Unit (Ctx, Current_Name, Kind);
+
+         --  The first iteration gives the unit we are required to return
+         if First_Unit = No_Analysis_Unit then
+            First_Unit := Unit;
+         end if;
+
+         --  Prepare semantic analysis and reference the unit from the current
+         --  unit for each mentionned unit.
+         if Root (Unit) /= null then
+            Populate_Lexical_Env (Unit);
+            Reference_Unit
+              (From => Get_Unit (Current_Name), Referenced => Unit);
+         end if;
+
+         --  Fetch the next mention name to process
+         if Current_Name.all in Base_Id_Type'Class then
+            Current_Name := null;
+         elsif Current_Name.all in Dotted_Name_Type'Class then
+            Current_Name := Ada_Node (Dotted_Name (Current_Name).F_Prefix);
+         else
+            raise Property_Error;
+         end if;
+      end loop;
+
+      return First_Unit;
    end Fetch_Unit;
 
 end Libadalang.Unit_Files;

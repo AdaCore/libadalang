2607cf3f8945510739708b558561ffd90d4ddd25
Raphael Amiard <amiard@adacore.com>
2017-06-18 09:27:09 +0200

    P506-012: Add test for generic fn call
    Change-Id: I4564ac182c673b21c011b8fa25aba6ecc60510ae

---
+++ ada/testsuite/tests/name_resolution/generic_fn_call/vector.ads
@@ -0,0 +1,8 @@
+generic
+   type Element_T is private;
+package Vector is
+   type Vector is null record;
+
+   function Element (Self : Vector; Index : Integer) return Element_T;
+   procedure Append  (Self : Vector; Element: Integer);
+end Vector;

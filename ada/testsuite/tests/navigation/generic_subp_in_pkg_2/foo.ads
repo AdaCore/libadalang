5d3b21a8d7097f4a02767d38027397a7f4a04d43
Raphael Amiard <amiard@adacore.com>
2017-06-28 16:59:12 +0200

    P506-012: Fix subp qualifier in test
    Change-Id: I29f39d786421391740e0b0d95f1a5d6f7f4e21d4

--- ada/testsuite/tests/navigation/generic_subp_in_pkg_2/foo.ads
+++ ada/testsuite/tests/navigation/generic_subp_in_pkg_2/foo.ads
@@ -4,7 +4,7 @@ package Foo is
 
    generic
       type T is private;
-      with procedure To_String (Inst : T) return String;
+      with function To_String (Inst : T) return String;
    procedure Print_T (Inst : T);
 
    procedure Print_Int is new Print_T (Integer, To_String);

a19011f7357cbb14849bc6c563bd1901cef47a3b
Pierre-Marie de Rodat <derodat@adacore.com>
2017-07-26 09:55:28 +0200

    name_resolution_ext/gnatcoll: fix style issue
    TN: Q724-018

--- ada/testsuite/tests/name_resolution_ext/gnatcoll/test.py
+++ ada/testsuite/tests/name_resolution_ext/gnatcoll/test.py
@@ -1,8 +1,6 @@
 from __future__ import absolute_import, division, print_function
 
-import os
-
-from utils import get_ext_src, get_gnatcoll_project_file, run_nameres
+from utils import get_gnatcoll_project_file, run_nameres
 
 
 run_nameres(['--quiet', '--files-from-project',

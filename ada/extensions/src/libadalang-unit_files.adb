c5b3c49bef5826ac215fbaa242df305e61d79cc3
Raphael Amiard <amiard@adacore.com>
2017-11-09 12:02:14 +0100

    P506-012: Refactor Fetch_Unit to work on array of symbols
    Change-Id: I67933dcfadb3cdef90606f10d6116b0a63579352

--- ada/extensions/src/libadalang-unit_files.adb
+++ ada/extensions/src/libadalang-unit_files.adb
@@ -1,19 +1,49 @@
 with Langkit_Support.Text; use Langkit_Support.Text;
 
-with Libadalang.Unit_Files.Default; use Libadalang.Unit_Files.Default;
-
 package body Libadalang.Unit_Files is
 
-   Text_IO        : constant String := "ada.text_io";
-   Integer_IO     : aliased constant String := "integer_io";
-   Float_IO       : aliased constant String := "float_io";
-   Fixed_IO       : aliased constant String := "fixed_io";
-   Decimal_IO     : aliased constant String := "decimal_io";
-   Enumeration_IO : aliased constant String := "enumeration_io";
-   Text_IO_Subpackages : constant array (Positive range <>)
-                                  of access constant String :=
-     (Integer_IO'Access, Float_IO'Access, Fixed_IO'Access, Decimal_IO'Access,
-      Enumeration_IO'Access);
+   Text_IO        : constant Text_Type := "ada.text_io";
+   Integer_IO     : aliased constant Text_Type := "integer_io";
+   Float_IO       : aliased constant Text_Type := "float_io";
+   Fixed_IO       : aliased constant Text_Type := "fixed_io";
+   Decimal_IO     : aliased constant Text_Type := "decimal_io";
+   Enumeration_IO : aliased constant Text_Type := "enumeration_io";
+
+   Text_IO_Subpackages :
+     constant array (Positive range <>) of access constant Text_Type 
+       := (Integer_IO'Access, Float_IO'Access, Fixed_IO'Access,
+           Decimal_IO'Access, Enumeration_IO'Access);
+
+   ---------------------
+   -- Name_To_Symbols --
+   ---------------------
+
+   function Name_To_Symbols (Name : Bare_Ada_Node) return Symbol_Type_Array is
+     (case Kind (Name) is
+
+      when Ada_Base_Id =>
+        (1 => Get_Symbol (F_Tok (Bare_Identifier (Name)))),
+
+      when Ada_Dotted_Name =>
+        Name_To_Symbols (Bare_Ada_Node (Bare_Dotted_Name (Name).F_Prefix))
+        & Name_To_Symbols
+           (Bare_Ada_Node (Bare_Dotted_Name (Name).F_Suffix)),
+
+      when others =>
+         raise Property_Error with "Wrong node in Name_To_Symbols");
+
+   ---------------
+   -- To_String --
+   ---------------
+
+   function To_String (Name : Symbol_Type_Array) return Text_Type
+   is
+      (if Name'Length > 0
+       then Name (Name'First).all
+            & (if Name'Length > 1
+               then "." & To_String (Name (Name'First + 1 .. Name'Last))
+               else "")
+       else "");
 
    ----------------
    -- Fetch_Unit --
@@ -23,6 +53,17 @@ package body Libadalang.Unit_Files is
      (Ctx  : Analysis_Context;
       Name : Bare_Ada_Node;
       Kind : Unit_Kind) return Analysis_Unit
+   is
+   begin
+      return Fetch_Unit
+        (Ctx, Name_To_Symbols (Name), Get_Unit (Create (Name)), Kind);
+   end Fetch_Unit;
+
+   function Fetch_Unit
+     (Ctx       : Analysis_Context;
+      Name      : Symbol_Type_Array;
+      From_Unit : Analysis_Unit;
+      Kind      : Unit_Kind) return Analysis_Unit
    is
       procedure Prepare_Semres (Unit : Analysis_Unit);
       --  Prepare semantic analysis and reference Unit from the current unit
@@ -35,66 +76,48 @@ package body Libadalang.Unit_Files is
       begin
          if not Root (Unit).Is_Null then
             Populate_Lexical_Env (Unit);
-            Reference_Unit (From       => Get_Unit (Create (Name)),
+            Reference_Unit (From       => From_Unit,
                             Referenced => Unit);
          end if;
       end Prepare_Semres;
 
-      UFP              : constant Unit_Provider_Access_Cst :=
-         Unit_Provider (Ctx);
-
+      UFP : constant Unit_Provider_Access_Cst := Unit_Provider (Ctx);
       Unit, First_Unit : Analysis_Unit;
-      Current_Name     : Bare_Ada_Node := Name;
-
    begin
 
+      --  GNAT kludge: as an "optimization", the generic subpackages in
+      --  Ada.Text_IO (see Text_IO_Subpackages) are not present in the
+      --  Ada.Text_IO unit itself, but in private child packages. GNAT
+      --  magically imports them in Ada.Text_IO's namespace.
+      --
+      --  Here, try to import these child unit as soon as someone WITHes
+      --  Ada.Text_IO.
+
+      if Kind = Unit_Specification and then To_String (Name) = Text_IO then
+         for SP of Text_IO_Subpackages loop
+            Prepare_Semres
+              (UFP.Get_Unit (Ctx, Text_IO & "." & SP.all, Kind));
+         end loop;
+      end if;
+
       --  In Ada, "with A.B" gives visibility to A and A.B. To process all
-      --  "mentionned" units, the following loop iterates on ["A.B", "A"].
+      --  "mentioned" units, the following loop iterates on ["A.B", "A"].
 
-      while Current_Name /= null loop
-         --  TODO??? Find a proper way to handle file not found, parsing error,
-         --  etc.
-         Unit := UFP.Get_Unit
-           (Ctx, Unit_Text_Name (Bare_Name (Current_Name)), Kind);
-         Prepare_Semres (Unit);
+      for I in reverse Name'Range loop
+         declare
+            Current_Name : Symbol_Type_Array := Name (Name'First .. I);
+         begin
+            --  TODO??? Find a proper way to handle file not found, parsing
+            --  error, etc.
+            Unit := UFP.Get_Unit (Ctx, To_String (Current_Name), Kind);
+            Prepare_Semres (Unit);
 
-         --  GNAT kludge: as an "optimization", the generic subpackages in
-         --  Ada.Text_IO (see Text_IO_Subpackages) are not present in the
-         --  Ada.Text_IO unit itself, but in private child packages. GNAT
-         --  magically imports them in Ada.Text_IO's namespace.
-         --
-         --  Here, try to import these child unit as soon as someone WITHes
-         --  Ada.Text_IO.
-
-         if Kind = Unit_Specification
-            and then Unit_String_Name (Bare_Name (Name)) = Text_IO
-         then
-            for SP of Text_IO_Subpackages loop
-               declare
-                  SU : constant Analysis_Unit := UFP.Get_Unit
-                    (Ctx, To_Text (Text_IO & "." & SP.all), Kind);
-               begin
-                  Prepare_Semres (SU);
-               end;
-            end loop;
-         end if;
-
-         --  The first iteration gives the unit we are required to return
-         if First_Unit = No_Analysis_Unit then
-            First_Unit := Unit;
-         end if;
-
-         --  Fetch the next name to process
-         if Current_Name.all in Bare_Base_Id_Type'Class then
-            Current_Name := null;
-         elsif Current_Name.all in Bare_Dotted_Name_Type'Class then
-            Current_Name :=
-               Bare_Ada_Node (Bare_Dotted_Name (Current_Name).F_Prefix);
-         else
-            raise Property_Error;
-         end if;
+            --  The first iteration gives the unit we are required to return
+            if First_Unit = No_Analysis_Unit then
+               First_Unit := Unit;
+            end if;
+         end;
       end loop;
-
       return First_Unit;
    end Fetch_Unit;
 
--- ada/extensions/src/libadalang-unit_files.ads
+++ ada/extensions/src/libadalang-unit_files.ads
@@ -1,16 +1,27 @@
+with Langkit_Support.Symbols; use Langkit_Support.Symbols;
+
 with Libadalang.Analysis; use Libadalang.Analysis;
 with Libadalang.Analysis.Implementation;
 use Libadalang.Analysis.Implementation;
 
 package Libadalang.Unit_Files is
 
+   type Symbol_Type_Array is array (Positive range <>) of Symbol_Type;
+
    function Fetch_Unit
      (Ctx  : Analysis_Context;
       Name : Bare_Ada_Node;
       Kind : Unit_Kind) return Analysis_Unit;
-   --  Fetch the unit for the file that (Name, Kind) designate. Populate
-   --  populate its lexical environment and reference the result from Name's
-   --  unit.
+
+   function Fetch_Unit
+     (Ctx       : Analysis_Context;
+      Name      : Symbol_Type_Array;
+      From_Unit : Analysis_Unit;
+      Kind      : Unit_Kind) return Analysis_Unit;
+
+   --  Fetch the unit for the file that (Name, Kind) designate. Populate its
+   --  lexical environment and reference the result from Name's unit.
+   --
    --
    --  When Name is an illegal unit name (a call expression, for instance),
    --  this raises a Property_Error.

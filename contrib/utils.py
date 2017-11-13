2a7ca4d65376007881de824c893f7ac491059026
Raphael Amiard <amiard@adacore.com>
2017-11-13 13:27:48 +0100

    P506-012: Add contrib/utils.py
    For GitHub issue #13
    
    Change-Id: I4855806b189f3e3720cd1f534046526177f969d0

---
+++ contrib/utils.py
@@ -0,0 +1,37 @@
+"""
+Grab bag of python utils/examples of use of the LAL python API.
+"""
+
+from __future__ import (absolute_import, division, print_function)
+
+import libadalang as lal
+
+
+def fqdn(basic_decl, child=None):
+    """
+    Return the fully qualified name of basic_decl.
+
+    :param lal.BasicDecl basic_decl: The basic declaration for which we want
+        the fully qualified name.
+    """
+
+    if not basic_decl:
+        return ""
+
+    def rel_name(n):
+        """
+        Return the relative name of n.
+
+        :param lal.Name n: The name.
+        """
+        return (n.text if n.is_a(lal.BaseId) else rel_name(n.f_suffix))
+
+    parent_name = fqdn(basic_decl.p_semantic_parent, basic_decl)
+
+    if child and child.is_a(lal.PackageBody):
+        return parent_name
+    else:
+        return ".".join(
+            el for el in [parent_name, rel_name(basic_decl.p_defining_name)]
+            if el
+        )

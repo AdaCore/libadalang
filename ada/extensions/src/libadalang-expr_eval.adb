8cac2c10c82c4ab32a519b733bce5bdd3601a988
Raphael Amiard <amiard@adacore.com>
2017-11-15 14:33:27 +0100

    P506-012 (#23): Add basic static expression evaluator
    Change-Id: Iad7670b73372c4c2c1d72c230e3840f555201168

---
+++ ada/extensions/src/libadalang-expr_eval.adb
@@ -0,0 +1,84 @@
+package body Libadalang.Expr_Eval is
+
+   ---------------
+   -- Expr_Eval --
+   ---------------
+
+   function Expr_Eval (E : LAL.Expr) return Eval_Result is
+
+      ---------------
+      -- Eval_Decl --
+      ---------------
+
+      function Eval_Decl (D : LAL.Basic_Decl) return Eval_Result is
+      begin
+         case D.Kind is
+            when LAL.Ada_Enum_Literal_Decl =>
+
+               --  An enum literal declaration evaluates to itself.
+               return (Enum_Lit,
+                       D.As_Enum_Literal_Decl.P_Enum_Type,
+                       D.As_Enum_Literal_Decl);
+
+            when LAL.Ada_Number_Decl =>
+
+               --  A number declaration evaluates to the evaluation of its
+               --  expression.
+               return Expr_Eval (D.As_Number_Decl.F_Expr);
+
+            when others =>
+               raise LAL.Property_Error;
+         end case;
+      end Eval_Decl;
+
+   begin
+      case E.Kind is
+         when LAL.Ada_Base_Id =>
+
+            return Eval_Decl (E.As_Base_Id.P_Referenced_Decl);
+
+         when LAL.Ada_Int_Literal =>
+            return (Int,
+                    E.P_Universal_Int_Type.As_Base_Type_Decl,
+                    Long_Integer'Value (LAL.Text (E.As_Int_Literal.F_Tok)));
+
+         when LAL.Ada_Real_Literal =>
+            return (Real,
+                    E.P_Universal_Real_Type.As_Base_Type_Decl,
+                    Long_Float'Value (LAL.Text (E.As_Real_Literal.F_Tok)));
+
+         when LAL.Ada_Bin_Op =>
+            raise LAL.Property_Error;
+         when others =>
+            raise LAL.Property_Error;
+      end case;
+   end Expr_Eval;
+
+   ------------
+   -- As_Int --
+   ------------
+
+   function As_Int (Self : Eval_Result) return Integer is
+   begin
+      case Self.Kind is
+         when Int => return Integer (Self.Int_Result);
+         when Real => raise LAL.Property_Error;
+         when Enum_Lit => return Self.Enum_Result.Child_Index + 1;
+      end case;
+   end As_Int;
+
+   -----------
+   -- Image --
+   -----------
+
+   function Image (Self : Eval_Result) return String is
+   begin
+      return "<Eval_Result "
+        & Self.Kind'Image & " "
+        & (case Self.Kind is
+              when Int => Self.Int_Result'Image,
+              when Real => Self.Real_Result'Image,
+              when Enum_Lit => Self.Enum_Result.Short_Image) & ">";
+   end Image;
+
+end Libadalang.Expr_Eval;
---
+++ ada/extensions/src/libadalang-expr_eval.ads
@@ -0,0 +1,39 @@
+with Libadalang.Analysis;
+
+--  This package implements an expression evaluator for Libadalang. The aim
+--  is to allow at least evaluation of expressions that fit the definitions
+--  of static expression in the Ada Reference Manual.
+
+package Libadalang.Expr_Eval is
+
+   package LAL renames Libadalang.Analysis;
+
+   type Expr_Kind is (Enum_Lit, Int, Real);  
+
+   type Eval_Result (Kind : Expr_Kind) is record
+      Expr_Type   : LAL.Base_Type_Decl;
+
+      case Kind is
+         when Enum_Lit =>
+            Enum_Result : LAL.Enum_Literal_Decl;
+         when Int =>
+            Int_Result  : Long_Integer;
+         when Real =>
+            Real_Result : Long_Float;
+      end case;
+   end record;
+   --  This data type represents the result of the evaluation of an expression
+   
+   function As_Int (Self : Eval_Result) return Integer;
+   --  Return the given evaluation result as an Integer, if applicable. This
+   --  will work for enum or int results, not for real results.
+   
+   function Image (Self : Eval_Result) return String;
+   --  Return a string representation of Self. Used for testing/debugging
+   --  purposes.
+
+   function Expr_Eval (E : LAL.Expr) return Eval_Result;
+   --  Evaluate the expression passed as parameter
+  
+
+end Libadalang.Expr_Eval;

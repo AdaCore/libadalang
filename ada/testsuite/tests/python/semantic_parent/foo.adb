85d89c5770842f99af1edc2cdfcfed5fed0d93d3
Raphael Amiard <amiard@adacore.com>
2017-11-17 11:52:27 +0100

    P506-012 (#13): Add test for semantic_parent

---
+++ ada/testsuite/tests/python/semantic_parent/foo.adb
@@ -0,0 +1,7 @@
+package body Foo is
+   procedure Bar (A : Integer) is
+      type Foo is null record;
+   begin
+      null;
+   end Bar;
+end Foo;
---
+++ ada/testsuite/tests/python/semantic_parent/test.py
@@ -0,0 +1,16 @@
+from __future__ import absolute_import, division, print_function
+
+"""
+Test that the p_semantic_parent property works as one would expect.
+"""
+
+import libadalang as lal
+
+c = lal.AnalysisContext('utf-8')
+u = c.get_from_file("foo.adb")
+
+ps = u.root.find(lal.ParamSpec)
+print(ps.p_semantic_parent)
+
+btd = u.root.find(lal.BaseTypeDecl)
+print(btd.p_semantic_parent)

c14f904453f47de88b5a2aa9750b879a2aeb8f1d
Pierre-Marie de Rodat <derodat@adacore.com>
2017-12-08 11:49:55 +0100

    highlight: generate executables in a separate directory
    TN: QC04-010

--- ada/testsuite/tests/contrib/highlight/test.py
+++ ada/testsuite/tests/contrib/highlight/test.py
@@ -7,6 +7,6 @@ from utils import gprbuild, in_contrib
 
 gprbuild(in_contrib('highlight', 'highlight.gpr'))
 subprocess.check_call(
-    [in_contrib('highlight', 'obj', 'highlight'), 'example.adb'],
+    [in_contrib('highlight', 'bin', 'highlight'), 'example.adb'],
     cwd=in_contrib('highlight')
 )

a8a78cbadb99cbfb3c09d785d7be382dc85cf230
Pierre-Marie de Rodat <derodat@adacore.com>
2018-01-05 12:08:58 +0100

    LAL.Unit_Files.Projects: remove unused abstraction in Get_Unit impl.
    TN: QC20-032

--- ada/extensions/src/libadalang-unit_files-projects.adb
+++ ada/extensions/src/libadalang-unit_files-projects.adb
@@ -4,31 +4,23 @@ with Libadalang.Unit_Files.Default;
 
 package body Libadalang.Unit_Files.Projects is
 
-   function Get_Unit
-     (Provider    : Project_Unit_Provider_Type'Class;
-      Context     : Analysis_Context;
-      Name        : String;
-      Kind        : Unit_Kind;
-      Charset     : String := "";
-      Reparse     : Boolean := False)
-      return Analysis_Unit;
-   --  Helper for Get_Unit primitives
-
    --------------
    -- Get_Unit --
    --------------
 
-   function Get_Unit
-     (Provider    : Project_Unit_Provider_Type'Class;
+   overriding function Get_Unit
+     (Provider    : Project_Unit_Provider_Type;
       Context     : Analysis_Context;
-      Name        : String;
+      Name        : Text_Type;
       Kind        : Unit_Kind;
       Charset     : String := "";
       Reparse     : Boolean := False) return Analysis_Unit
    is
-      File : constant Filesystem_String := File_From_Unit
+      Str_Name : constant String :=
+         Libadalang.Unit_Files.Default.Unit_String_Name (Name);
+      File     : constant Filesystem_String := File_From_Unit
         (Project   => Root_Project (Provider.Project.all),
-         Unit_Name => Name,
+         Unit_Name => Str_Name,
          Part      => Convert (Kind),
          Language  => "Ada");
    begin
@@ -44,34 +36,17 @@ package body Libadalang.Unit_Files.Projects is
 
       declare
          Dummy_File : constant String :=
-            Libadalang.Unit_Files.Default.File_From_Unit (Name, Kind);
+            Libadalang.Unit_Files.Default.File_From_Unit (Str_Name, Kind);
          Kind_Name  : constant String :=
            (case Kind is
             when Unit_Specification => "specification file",
             when Unit_Body          => "body file");
          Error      : constant String :=
-            "Could not find source file for " & Name & " (" & Kind_Name & ")";
+            "Could not find source file for " & Str_Name & " (" & Kind_Name
+            & ")";
       begin
          return Get_With_Error (Context, Dummy_File, Error, Charset);
       end;
-   end Get_Unit;
-
-   --------------
-   -- Get_Unit --
-   --------------
-
-   overriding function Get_Unit
-     (Provider    : Project_Unit_Provider_Type;
-      Context     : Analysis_Context;
-      Name        : Text_Type;
-      Kind        : Unit_Kind;
-      Charset     : String := "";
-      Reparse     : Boolean := False) return Analysis_Unit
-   is
-      Str_Name : constant String :=
-         Libadalang.Unit_Files.Default.Unit_String_Name (Name);
-   begin
-      return Get_Unit (Provider, Context, Str_Name, Kind, Charset, Reparse);
    end Get_Unit;
 
    ----------------

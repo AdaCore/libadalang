b29937597327a33aad75038446854ca3918fcf1b
Pierre-Marie de Rodat <derodat@adacore.com>
2018-04-03 17:33:44 +0200

    Add a regression testcase for R403-028
    TN: R403-028

---
+++ ada/testsuite/tests/regressions/R403-028/test.py
@@ -0,0 +1,11 @@
+from __future__ import absolute_import, division, print_function
+
+import libadalang as lal
+
+ctx = lal.AnalysisContext(with_trivia=True)
+unit = ctx.get_from_file('lal-highlighters.adb')
+loc = lal.Sloc(1, 1)
+t = unit.lookup_token(loc)
+for i in range(18):
+    print(t)
+    t = t.next

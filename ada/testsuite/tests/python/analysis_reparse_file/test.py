34454a81e6aa8695533674d8abdfe178454e4ae8
Pierre-Marie de Rodat <derodat@adacore.com>
2018-06-12 11:00:01 +0200

    Update testsuite baselines after recent Langkit update
    TN: R608-028

--- ada/testsuite/tests/python/analysis_reparse_file/test.py
+++ ada/testsuite/tests/python/analysis_reparse_file/test.py
@@ -30,9 +30,6 @@ def check(unit):
     if unit.diagnostics:
         print('Diagnostics for foo.adb:')
         for diag in unit.diagnostics:
-            diag = str(diag)
-            if diag.startswith('Cannot open '):
-                diag = 'Cannot open <somefile>'
             print('  {}'.format(diag))
     else:
         print('WithClause: has_limited = {}'.format(

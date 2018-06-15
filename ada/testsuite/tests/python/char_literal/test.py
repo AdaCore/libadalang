699923faa7618c033c76629f02d9078a38b4da3a
Pierre-Marie de Rodat <derodat@adacore.com>
2018-06-15 12:28:08 +0200

    python/char_literal: new testcase
    TN: R530-015

---
+++ ada/testsuite/tests/python/char_literal/test.py
@@ -0,0 +1,21 @@
+"""
+Test that CharLiteral.p_denoted_value properly decodes all valid Ada character
+literals.
+"""
+
+from __future__ import absolute_import, division, print_function
+
+import libadalang as lal
+
+c = lal.AnalysisContext('utf-8')
+u = c.get_from_file('foo.ads')
+
+for decl in u.root.findall(lal.ObjectDecl):
+    name = decl.f_ids.text
+    expr = decl.f_default_expr
+    assert isinstance(expr, lal.CharLiteral)
+    try:
+        v = expr.p_denoted_value
+    except lal.PropertyError:
+        v = '<PropertyError>'
+    print('{} ({}) -> {}'.format(name, repr(expr.text), repr(v)))

b331566a2a45bfe0fcbfe31e97c960f6d89f41e1
Pierre-Marie de Rodat <derodat@adacore.com>
2018-06-27 11:24:07 +0200

    python/int_literal: enhance testing of non-decimal bases
    TN: R625-013

--- ada/testsuite/tests/python/int_literal/foo.ads
+++ ada/testsuite/tests/python/int_literal/foo.ads
@@ -15,9 +15,14 @@ package Foo is
    Base2_4 : constant := 2#1010_1111#;
    Base2_5 : constant := 2:1010_1111:;
    Base2_6 : constant := 2#10#E20;
+   Base2_7 : constant := 2#2#; --  Invalid
 
    Base7_1  : constant := 7#10#;
    Base16_1 : constant := 16#10#;
+   Base16_2 : constant := 16#1f#;
+   Base16_3 : constant := 16#Ff#;
+
+   Base17_1 : constant := 17#10#; --  Invalid
 
    Base_OOR_1 : constant := 100000000000000000000#10#; --  Rejected, too big
 

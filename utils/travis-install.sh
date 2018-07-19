58ac77eff396bf150a542d2853cdd3b82f74449f
Pierre-Marie de Rodat <derodat@adacore.com>
2018-07-19 15:44:03 +0200

    Fix the Travis CI setup to work with GNAT Community 2018

--- utils/travis-install.sh
+++ utils/travis-install.sh
@@ -3,64 +3,103 @@
 set -v
 set -e
 
-# Get GNAT
-if ! test -f $GNAT_TAR_PATH
+if ! [ -d $TOOLS_DIR ]
 then
     mkdir -p $TOOLS_DIR
-    wget -O $GNAT_TAR_PATH \
-        "http://mirrors.cdn.adacore.com/art/591c6d80c7a447af2deed1d7"
+fi
+if ! [ -d $INSTALL_DIR ]
+then
+    mkdir -p $INSTALL_DIR
+fi
+
+# Get and install GNAT
+if ! [ -d gnat_community_install_script ]
+then
+    git clone https://github.com/AdaCore/gnat_community_install_script.git
+else
+    (cd gnat_community_install_script && git pull)
+fi
+if ! [ -f $GNAT_INSTALLER ]
+then
+    wget -O $GNAT_INSTALLER \
+        http://mirrors.cdn.adacore.com/art/5b0d7bffa3f5d709751e3e04
+    sh gnat_community_install_script/install_package.sh \
+        "$GNAT_INSTALLER" "$INSTALL_DIR"
 fi
 
 # Get QUEX
-if ! test -f $QUEX_ZIP_PATH
+if ! [ -d $QUEX_PATH ]
 then
-    wget -O $QUEX_ZIP_PATH \
+    wget -O $QUEX_ZIP \
         "https://downloads.sourceforge.net/project/quex/HISTORY/0.65/quex-0.65.4.zip?r=https%3A%2F%2Fsourceforge.net%2Fprojects%2Fquex%2Ffiles%2FHISTORY%2F0.65%2F&ts=1484909333&use_mirror=heanet"
+    unzip -o $QUEX_ZIP -d $TOOLS_DIR
 fi
 
-# Get GNATCOLL
-if ! test -f $GNATCOLL_TAR_PATH
+# Get gnatcoll-core and gnatcoll-bindings
+if [ -d "$TOOLS_DIR/gnatcoll-core" ]
 then
-    wget -O $GNATCOLL_TAR_PATH \
-        "http://mirrors.cdn.adacore.com/art/5a15c79cc7a4479a23674c66"
+    (cd $TOOLS_DIR/gnatcoll-core && git pull)
+else
+    (cd $TOOLS_DIR && git clone https://github.com/AdaCore/gnatcoll-core)
 fi
-
-# If needed, extract QUEX, GNAT and GNATCOLL
-if ! test -d $GNAT_PATH
+if [ -d "$TOOLS_DIR/gnatcoll-bindings" ]
 then
-    tar -xf $GNAT_TAR_PATH -C $TOOLS_DIR
-fi
-if ! test -d $QUEX_PATH
-then
-    unzip -o $QUEX_ZIP_PATH -d $TOOLS_DIR
-fi
-if ! test -d $GNATCOLL_PATH
-then
-    tar xf $GNATCOLL_TAR_PATH -C $TOOLS_DIR
+    (cd $TOOLS_DIR/gnatcoll-bindings && git pull)
+else
+    (cd $TOOLS_DIR && git clone https://github.com/AdaCore/gnatcoll-bindings)
 fi
 
 # Get Langkit
-if ! test -d langkit
+if ! [ -d langkit ]
 then
     git clone https://github.com/AdaCore/langkit
+else
+    (cd langkit && git pull)
 fi
-(cd langkit && git pull --rebase origin master)
 
 # Install requirements
 pip install -r langkit/REQUIREMENTS.dev
 
-# Log content
-ls $HOME/build_tools
-ls $GNAT_PATH
-ls $GNAT_PATH/bin
-ls $QUEX_PATH
+# TODO: the following is a hack to workaround the fact that 1) in Ubuntu,
+# libiconv is integrated in the libc, so it's not present as a standalone
+# library, 2) the build of gnatcoll-iconv passes -liconv to the linker.
+LIBICONV_DIR=$TOOLS_DIR/libiconv-hack
+rm -rf $LIBICONV_DIR
+mkdir $LIBICONV_DIR
+(
+    cd $LIBICONV_DIR
+    touch libiconv.c
+    gcc -shared -o libiconv.so libiconv.c
+)
+if [ -z "$LIBRARY_PATH" ]
+then
+    export LIBRARY_PATH=$LIBICONV_DIR
+else
+    export LIBRARY_PATH="$LIBICONV_DIR:$LIBRARY_PATH"
+fi
 
-# Build GNATCOLL
+# Log content
 pwd
-export PATH=$GNAT_PATH/bin:$PATH
-cd $GNATCOLL_PATH
+export PATH=$INSTALL_DIR/bin:$PATH
 which gcc
-./configure --prefix="$GNAT_PATH" --enable-build=Debug --enable-shared --enable-projects
-make PROCESSORS=0
-make install
-cd -
+gcc -v
+
+# Build gnatcoll-core
+(
+    cd $TOOLS_DIR/gnatcoll-core
+    make build PROCESSORS=0
+    make install prefix="$INSTALL_DIR"
+)
+
+# Build gnatcoll-bindings
+(
+    cd $TOOLS_DIR/gnatcoll-bindings
+    for component in iconv gmp
+    do
+        (
+            cd $component
+            python setup.py build -j0 --prefix="$INSTALL_DIR"
+            python setup.py install --prefix="$INSTALL_DIR"
+        )
+    done
+)
--- utils/travis-script.sh
+++ utils/travis-script.sh
@@ -3,7 +3,25 @@
 set -v
 set -e
 
-export PATH=$GNAT_PATH/bin:$PATH
-gprbuild --version
+export PATH=$INSTALL_DIR/bin:$PATH
+
+# See the corresponding code in travis-install.sh
+LIBICONV_DIR=$TOOLS_DIR/libiconv-hack
+if [ -z "$LIBRARY_PATH" ]
+then
+    export LIBRARY_PATH=$LIBICONV_DIR
+else
+    export LIBRARY_PATH="$LIBICONV_DIR:$LIBRARY_PATH"
+fi
+
+# Log the toolchain to use
+which gcc
+which gprbuild
+gcc -v
+gprbuild -v
+
+# Log which Langkit commit is used
+(cd langkit && git log HEAD^..HEAD | cat)
+
 ada/manage.py make -j16
 ada/manage.py test

0087c7d4be0a904899d7252cc7f9014d79367b2b
Pierre-Marie de Rodat <derodat@adacore.com>
2018-07-23 12:24:14 +0200

    Travis CI: build quex_lexer.c separately

--- utils/travis-script.sh
+++ utils/travis-script.sh
@@ -29,5 +29,16 @@ gprbuild -v
 # Log which Langkit commit is used
 (cd langkit && git log HEAD^..HEAD | cat)
 
-ada/manage.py make -j16
+# Avoid pretty-printing, for now: gnatpp from GNAT Community 2018 is known not
+# to work on Libadalang.
+ada/manage.py generate -P
+
+# Build the Quex-generated lexer alone first, as it takes a huge amount of
+# memory. Only then build the rest in parallel.
+gprbuild -p -j8 -Pbuild/lib/gnat/libadalang.gpr \
+    -XBUILD_MODE=dev -XLIBRARY_TYPE=relocatable -XXMLADA_BUILD=relocatable \
+    -XLIBADALANG_WARNINGS=true -c -u quex_lexer.c
+ada/manage.py build
+
+# Finally, run the testsuite
 ada/manage.py test

9f28ea6641fd1684bfe7c94aebba8f88c4d11dd8
Pierre-Marie de Rodat <derodat@adacore.com>
2018-09-14 14:26:39 +0200

    User Manual: switch to the "sphinxdoc" theme by default
    TN: R601-021

--- user_manual/conf.py
+++ user_manual/conf.py
@@ -100,12 +100,13 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-try:
+import os
+if os.environ.get('USE_SPHINX_RTD_THEME'):
     import sphinx_rtd_theme
     html_theme = "sphinx_rtd_theme"
     html_theme_path = sphinx_rtd_theme.get_html_theme_path()
-except ImportError:
-    html_theme = 'default'
+else:
+    html_theme = 'sphinxdoc'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the

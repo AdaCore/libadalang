f8fd089c98fc6e9cd69faa67b7216c8e5d76b61f
Pierre-Marie de Rodat <derodat@adacore.com>
2018-09-18 12:44:52 +0200

    Add runtime exception to copyright headers
    TN: R411-040

--- ada/extensions/src/libadalang-sources.adb
+++ ada/extensions/src/libadalang-sources.adb
@@ -9,11 +9,16 @@
 -- Software Foundation;  either version 3,  or (at your option)  any later  --
 -- version.   This  software  is distributed in the hope that it  will  be  --
 -- useful but  WITHOUT ANY WARRANTY;  without even the implied warranty of  --
--- MERCHANTABILITY  or  FITNESS  FOR  A PARTICULAR PURPOSE.   See the  GNU  --
--- General  Public  License for more details.   You should have received a  --
--- copy of the GNU General Public License  distributed with this software;  --
--- see  file  COPYING3.  If not, go to  http://www.gnu.org/licenses  for a  --
--- complete copy of the license.                                            --
+-- MERCHANTABILITY  or  FITNESS  FOR  A PARTICULAR PURPOSE.                 --
+--                                                                          --
+-- As a special  exception  under  Section 7  of  GPL  version 3,  you are  --
+-- granted additional  permissions described in the  GCC  Runtime  Library  --
+-- Exception, version 3.1, as published by the Free Software Foundation.    --
+--                                                                          --
+-- You should have received a copy of the GNU General Public License and a  --
+-- copy of the GCC Runtime Library Exception along with this program;  see  --
+-- the files COPYING3 and COPYING.RUNTIME respectively.  If not, see        --
+-- <http://www.gnu.org/licenses/>.                                          --
 ------------------------------------------------------------------------------
 
 with Ada.Wide_Wide_Characters.Handling;

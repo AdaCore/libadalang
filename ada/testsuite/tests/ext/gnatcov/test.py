032a5ee3fe648b85ac7e2357b1437953b5b1080a
Pierre-Marie de Rodat <derodat@adacore.com>
2019-02-19 16:48:34 +0100

    ext/gnatcov: do not discard the previous GPR_PROJECT_PATH
    TN: S219-030

--- ada/testsuite/tests/ext/gnatcov/test.py
+++ ada/testsuite/tests/ext/gnatcov/test.py
@@ -11,7 +11,13 @@ gnatcov_dir = get_ext_src('gnatcoverage')
 # src/gnatcoll.gpr exists.
 gnatcoll_dir = os.path.join(get_ext_src('gnatcoll'), 'src')
 
-os.environ['GPR_PROJECT_PATH'] = gnatcoll_dir
+# Insert items in GPR_PROJECT_PATH but do not discard its previous value, as
+# GNATcoverage depends on Libadalang.
+old_path = os.environ.get('GPR_PROJECT_PATH', '')
+os.environ['GPR_PROJECT_PATH'] = (
+    '{}{}{}'.format(gnatcoll_dir, os.path.pathsep, old_path)
+    if old_path else gnatcoll_dir)
+
 run_nameres(['--only-show-failures', '--files-from-project', '--all',
              '-P{}'.format(os.path.join(gnatcov_dir, 'gnatcov.gpr')),
              '-XBINUTILS_SRC_DIR=/doesnotexists',

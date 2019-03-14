28aa3fff61bccf532fa0941b2bf9718f6346d6d1
Raphaël AMIARD <raph.amiard@gmail.com>
2019-03-14 11:41:41 +0100

    S215-003: Use Test_Statement_UID in tests depending on the runtime

--- ada/testsuite/tests/name_resolution/text_io_child_units/foo.adb
+++ ada/testsuite/tests/name_resolution/text_io_child_units/foo.adb
@@ -1,13 +1,16 @@
 with Ada.Text_IO;
+with Ada.Text_IO.Decimal_IO;
+pragma Test_Statement_UID;
+with Ada.Text_IO.Enumeration_IO;
+pragma Test_Statement_UID;
+with Ada.Text_IO.Fixed_IO;
+pragma Test_Statement_UID;
+with Ada.Text_IO.Float_IO;
+pragma Test_Statement_UID;
+with Ada.Text_IO.Integer_IO;
+pragma Test_Statement_UID;
 
 procedure Foo is
-   pragma Config (Display_Short_Images => True);
-
-   pragma Test (Ada.Text_IO.Integer_IO);
-   pragma Test (Ada.Text_IO.Float_IO);
-   pragma Test (Ada.Text_IO.Fixed_IO);
-   pragma Test (Ada.Text_IO.Decimal_IO);
-   pragma Test (Ada.Text_IO.Enumeration_IO);
 begin
    null;
 end Foo;

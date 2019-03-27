907fecaeca142658bb83825eb3a539ad4bdf4219
Romain Beguet <beguet@adacore.com>
2019-03-27 12:39:18 +0100

    S326-012: Only keep two digits of precision when displaying real values.
    Floating points computations are platform-dependent, so make sure we only
    display a few digits to lower the risks of having diffs.

--- ada/testsuite/tests/ada_api/static_expr_eval/main.adb
+++ ada/testsuite/tests/ada_api/static_expr_eval/main.adb
@@ -10,17 +10,54 @@ procedure Main is
    Ctx  : constant Analysis_Context := Create_Context;
    Unit : constant Analysis_Unit := Get_From_File (Ctx, "test.adb");
 
+   Float_Precision : constant := 2;
+
    function Is_Object_Decl (N : Ada_Node) return Boolean
    is (Kind (N) in Ada_Object_Decl
          and then not N.As_Object_Decl.F_Default_Expr.Is_Null);
+
+   function Eval_Result_To_String (X : Eval_Result) return String is
+   begin
+      if X.Kind = Real then
+         declare
+            Img     : String  := X.Real_Result'Image;
+
+            -- Get the integer value of the exponent. The exponent
+            -- is made of the last two characters of the image.
+            Exp     : Integer :=
+               Integer'Value (Img (Img'Last - 1 .. Img'Last));
+
+            -- Is the exponent positive?
+            Exp_Pos : Boolean := (Img (Img'Last - 2) = '+');
+
+            -- Compute the number of digits to keep in the final
+            -- representation so as to have exactly `Float_Precision` digits
+            -- of precision.
+            Length  : Integer := Float_Precision
+               + (if Exp_Pos then Exp else 0);
+         begin
+            -- Build the final string as the concatenation of:
+            --  * The first `3 + Length` characters of the original image
+            --    (the sign, the first digit and the dot + the computed
+            --    precision).
+            --  * The exponent of the original image.
+            return Img (1 .. 3 + Length) & Img (Img'Last - 3 .. Img'Last);
+         end;
+      else
+         return Image (X);
+      end if;
+   end Eval_Result_To_String;
+
 begin
    for E of Find (Root (Unit), Is_Object_Decl'Access).Consume loop
       begin
          declare
             Res : Eval_Result := Expr_Eval (E.As_Object_Decl.F_Default_Expr);
+            Img : String := Image (Res);
          begin
             Put_Line
-               ("Expr " & Short_Image (E) & " evaluated to " & Image (Res));
+               ("Expr " & Short_Image (E) & " evaluated to "
+                & Eval_Result_To_String (Res));
             if Res.Kind in Int | Enum_Lit then
                Put_Line ("   Int value is " & As_Int (Res).Image);
             end if;

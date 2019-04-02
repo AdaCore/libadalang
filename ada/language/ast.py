29ce1284dd3bad7f9e0c58c279a6e16312132a1e
Romain Beguet <beguet@adacore.com>
2019-04-02 11:47:13 +0200

    S329-049: Consider defining names.

--- ada/language/ast.py
+++ ada/language/ast.py
@@ -7426,14 +7426,14 @@ class DefiningName(Name):
 
     @langkit_property(public=True, return_type=AdaNode.entity.array,
                       dynamic_vars=[default_imprecise_fallback()])
-    def find_all_refs_in(x=AdaNode.entity):
+    def find_all_refs_in(x=AdaNode.entity, origin=AdaNode.entity):
         """
         Searches all references to this defining name in the given node and its
         children.
         """
         return x.children.then(
-            lambda c: c.filter(lambda n: Not(n.is_null | n.is_a(DefiningName)))
-            .mapcat(lambda n: Self.find_all_refs_in(n))
+            lambda c: c.filter(lambda n: Not(n.is_null | (n == origin)))
+            .mapcat(lambda n: Self.find_all_refs_in(n, origin))
         ).concat(If(
             (x.match(lambda i=BaseId: Self.name_is(i.name_symbol),
                      lambda _:        False)) &
@@ -7452,7 +7452,7 @@ class DefiningName(Name):
         dn = Var(Entity.canonical_part)
         return dn.filter_is_imported_by(units, True).mapcat(
             lambda u: u.root.then(
-                lambda r: dn.find_all_refs_in(r.as_entity)
+                lambda r: dn.find_all_refs_in(r.as_entity, Entity)
             )
         )
 

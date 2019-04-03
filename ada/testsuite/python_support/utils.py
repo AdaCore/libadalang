2e2f93c72e06639e72d47d230657ccbe54cf0587
Pierre-Marie de Rodat <derodat@adacore.com>
2019-04-03 16:25:18 +0200

    Testsuite: remove obsolete support for external sources
    TN: S328-049

--- ada/testsuite/python_support/utils.py
+++ ada/testsuite/python_support/utils.py
@@ -23,38 +23,6 @@ def in_contrib(*args):
     return os.path.join(LAL_ROOTDIR, 'contrib', *args)
 
 
-def get_ext_src(repo):
-    """
-    Return the absolute path to the external sources for repository "repo".
-
-    :param str repo: Name of the source repository.
-    :rtype: str
-    """
-    path = os.path.join(LAL_ROOTDIR, 'ada', 'testsuite', 'ext_src', repo)
-    assert os.path.isdir(path)
-    return path
-
-
-def get_gnatcoll_project_file():
-    """
-    Return the absolute path to GNATCOLL's project file.
-
-    For convenience in infrastructure-based testsuite runs, we make it possible
-    for GNATCOLL sources to be outside of the testsuite tree: in this case, we
-    expect the infrastructure to specify where the "gnatcoll.gpr" project file
-    lies.
-
-    :rtype: str
-    """
-    gnatcoll_dir = get_ext_src('gnatcoll')
-    fn = os.path.join(gnatcoll_dir, 'project_file_path.txt')
-    if os.path.exists(fn):
-        with open(fn) as f:
-            return f.read().strip()
-    else:
-        return os.path.join(gnatcoll_dir, 'gnatcoll.gpr')
-
-
 def gprbuild(project_file):
     """
     Invoke gprbuild on the given project file.

23b1abeaf1d3c53d09796eae733832e8448dd8f0
Pierre-Marie de Rodat <derodat@adacore.com>
2019-04-15 12:10:55 +0200

    Turn laldoc into an installable Python package
    TN: S402-029

---
+++ contrib/laldoc/setup.py
@@ -0,0 +1,19 @@
+#! /usr/bin/env python
+
+"""Setup configuration file for laldoc."""
+
+from __future__ import absolute_import, division, print_function
+
+from distutils.core import setup
+
+
+# Run the setup tools
+setup(
+    name='laldoc',
+    version='0.1-dev',
+    author='AdaCore',
+    author_email='report@adacore.com',
+    url='https://www.adacore.com',
+    description='Libadalang-based Ada source extractor for documentation',
+    packages=['laldoc']
+)

3fb52f88a6bc50ea0a1a65fc814e2cdb92ab537f
Romain Beguet <beguet@adacore.com>
2019-05-20 11:23:33 +0200

    S516-030: Fix is_fixed_point property.

---
+++ ada/testsuite/tests/properties/is_fixed_point/test.py
@@ -0,0 +1,20 @@
+from __future__ import absolute_import, division, print_function
+
+import sys
+
+import libadalang as lal
+
+
+for filename in sys.argv[1:]:
+    print('== {} =='.format(filename))
+    u = lal.AnalysisContext().get_from_file(filename)
+    assert not u.diagnostics
+
+    for tdecl in u.root.findall(lal.BaseTypeDecl):
+        print("{} {} a fixed point type.".format(
+            tdecl, "is" if tdecl.p_is_fixed_point else "is not"
+        ))
+
+    print('')
+
+print('Done')

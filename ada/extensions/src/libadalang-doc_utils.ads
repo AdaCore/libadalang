b29a0eb5d252e53e41a338e5bbbfd479350111f0
Raphaël AMIARD <raph.amiard@gmail.com>
2019-06-05 23:41:08 +0200

    S529-023: Add comments to LAL.Doc_Utils

--- ada/extensions/src/libadalang-doc_utils.ads
+++ ada/extensions/src/libadalang-doc_utils.ads
@@ -37,14 +37,19 @@ package Libadalang.Doc_Utils is
       (SSize            => GNATCOLL.Strings_Impl.Optimal_String_Size,
        Character_Type   => Wide_Wide_Character,
        Character_String => Wide_Wide_String);
+   --  Wide_Wide XString type instantiation. TODO??? Would probably make sense
+   --  to put that in Langkit_Support.
 
    package Annotations_Maps is new Ada.Containers.Indefinite_Hashed_Maps
      (Text_Type, Text_Type, Ada.Strings.Wide_Wide_Hash, "=");
 
    type Doc_Type is record
-      Doc : XStrings.XString;
+      Doc         : XStrings.XString;
+      --  Documentation, where every line is concatenated as one XString
       Annotations : Annotations_Maps.Map;
+      --  Annotations, as a key-value map of strings
    end record;
+   --  Type representing the documentation of an entity
 
    function Get_Documentation (Decl : Basic_Decl) return Doc_Type;
    --  Return the documentation for given Basic_Decl

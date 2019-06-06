f17c2bb8c68291a6cffde417bc4bd1100a8143cf
Pierre-Marie de Rodat <derodat@adacore.com>
2019-06-06 16:08:01 +0200

    laldoc: minor refactoring
    TN: S529-023

--- contrib/laldoc/laldoc/__init__.py
+++ contrib/laldoc/laldoc/__init__.py
@@ -365,8 +365,8 @@ class AutoPackage(Directive):
         # Go through all declarations that appear in the top-level package and
         # organize them in sections the way we want to document them.
 
-        decls = list(d for d in package_decl.f_public_part.f_decls
-                     if d.is_a(lal.BasicDecl))
+        decls = [d for d in package_decl.f_public_part.f_decls
+                 if d.is_a(lal.BasicDecl)]
 
         types = {}
 

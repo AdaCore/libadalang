aa5ac41dd7daae566bf5a8c5056981fec2bce93b
Romain Beguet <beguet@adacore.com>
2019-06-19 11:59:35 +0200

    S619-011: Rework is_call testcase.
    Include cases that do not work yet but should in the future.

--- ada/testsuite/tests/properties/is_call/test.py
+++ ada/testsuite/tests/properties/is_call/test.py
@@ -10,14 +10,31 @@ for filename in sys.argv[1:]:
     u = lal.AnalysisContext().get_from_file(filename)
     assert not u.diagnostics
 
-    print("Is call    Entity")
-    print("-" * 50)
-    for n in u.root.findall(lal.Name):
-        if not n.p_is_defining:
-            print('{}        {}'.format(
-                'YES' if n.p_is_call else 'NO ',
-                n
+    whole_src = u.text.splitlines()
+
+    def extract_sloc(sloc_range):
+        f = sloc_range.start
+        l = sloc_range.end
+        return (f.line, f.column), (l.line, l.column)
+
+    def format_output(s_l, s_c, e_l, e_c):
+        lines = whole_src[s_l - 1:e_l]
+        underline_starts = [s_c - 1] + [0] * (len(lines) - 1)
+        underline_ends = [len(l) for l in lines[:-1]] + [e_c - 1]
+        out = list(lines)
+        for i in range(len(lines)):
+            out.insert(i * 2 + 1, "{}{}".format(
+                " " * underline_starts[i],
+                "^" * (underline_ends[i] - underline_starts[i])
             ))
+
+        return "\n".join(out)
+
+    print('')
+    for n in u.root.findall(lal.Name):
+        if n.p_is_call:
+            (s_l, s_c), (e_l, e_c) = extract_sloc(n.sloc_range)
+            print(format_output(s_l, s_c, e_l, e_c))
 
     print('')
 

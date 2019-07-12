4e1f04dbc570256b47a62161a9cf390af7eee3d4
Raphaël AMIARD <raph.amiard@gmail.com>
2019-07-12 17:19:16 +0200

    S709-040: Add test for task nav

---
+++ ada/testsuite/tests/navigation/task_nav/test.py
@@ -0,0 +1,26 @@
+from __future__ import absolute_import, division, print_function
+
+import libadalang as lal
+
+ctx = lal.AnalysisContext()
+
+p_body = ctx.get_from_file("pack.adb")
+
+some_task = p_body.root.find(lal.TaskBody)
+
+print("some_task:", some_task)
+print("previous part:", some_task.p_previous_part_for_decl)
+print("canonical part:", some_task.p_canonical_part)
+print(
+    "canonical_part.next_part:",
+    some_task.p_canonical_part.p_next_part_for_decl
+)
+print(
+    "canonical_part.next_part.next_part:",
+    some_task.p_canonical_part.p_next_part_for_decl.p_next_part_for_decl
+)
+print(
+    "canonical_part.next_part.next_part.next_part:",
+    some_task.p_canonical_part.p_next_part_for_decl
+    .p_next_part_for_decl.p_next_part_for_decl
+)

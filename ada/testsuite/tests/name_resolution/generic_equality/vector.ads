683474f3ec2a41fa908fd813103c841a4b89187e
Romain Beguet <beguet@adacore.com>
2019-08-06 14:50:17 +0200

    Minor: Fix sources of test case.

--- ada/testsuite/tests/name_resolution/generic_equality/vector.ads
+++ ada/testsuite/tests/name_resolution/generic_equality/vector.ads
@@ -2,7 +2,6 @@ generic
    type Element_T is private;
 package Vector is
    type Vector is private;
-
-   function Element (Self : Vector; Index : Integer) return Element_T;
-   procedure Append  (Self : Vector; Element: Integer) return Element_T;
+private
+   type Vector is null record;
 end Vector;

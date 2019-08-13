b7e995f4b95c0f4d633574452a6560b225a048c3
Romain Beguet <beguet@adacore.com>
2019-08-13 17:00:43 +0200

    S412-019: Implement get_primitives.

---
+++ ada/testsuite/tests/properties/inherited_primitives/test.py
@@ -0,0 +1,19 @@
+from __future__ import absolute_import, division, print_function
+
+import libadalang as lal
+
+ctx = lal.AnalysisContext()
+u = ctx.get_from_file("test.adb")
+
+assert not u.diagnostics
+
+types = u.root.findall(
+    lambda n: n.is_a(lal.TypeDecl) and n.p_defining_name.text == "U"
+)
+
+for tpe in types:
+    print("Primitives inherited by {}:".format(tpe))
+    for p in tpe.p_get_primitives(False):
+        print("  {}".format(p))
+
+print('Done')

d7cedbfa17f58482ffe6916a3b043a6302479ce2
Raphaël AMIARD <raph.amiard@gmail.com>
2019-08-16 11:00:41 +0200

    S711-059: Rework get_aspect property

--- ada/testsuite/tests/properties/get_aspect_2/test.py
+++ ada/testsuite/tests/properties/get_aspect_2/test.py
@@ -11,9 +11,9 @@ for filename in sys.argv[1:]:
     assert not u.diagnostics
 
     for n in u.root.findall(lal.PackageDecl):
-        print("{} get_attribute('preelaborate') -> {}".format(
+        print("{} get_aspect('preelaborate') -> {}".format(
             n,
-            n.p_get_attribute('preelaborate'),
+            n.p_get_aspect('preelaborate'),
         ))
 
     print('')

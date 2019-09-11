89cab5e0496cb784e0dc7406a38e10a4e2a9d0ed
Raphaël AMIARD <raph.amiard@gmail.com>
2019-09-11 12:49:42 +0200

    GH #324: fix minor style issue

--- ada/extensions/src/libadalang-expr_eval.adb
+++ ada/extensions/src/libadalang-expr_eval.adb
@@ -191,8 +191,8 @@ package body Libadalang.Expr_Eval is
                end if;
 
             when others =>
-               raise Property_Error with "Cannot transform decl " &
-                  D.Kind'Image;
+               raise Property_Error
+                 with "Cannot eval decl " & D.Kind'Image;
          end case;
       end Eval_Decl;
 

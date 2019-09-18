5b1ad2077e71dcd6777bd39e49c0583cb1eab9df
Raphaël AMIARD <raph.amiard@gmail.com>
2019-09-18 11:13:33 +0200

    S725-022: get rid of call_non_memoizable

--- ada/language/ast.py
+++ ada/language/ast.py
@@ -436,8 +436,12 @@ class AdaNode(ASTNode):
     @langkit_property(
         return_type=AnalysisUnit, external=True, uses_entity_info=False,
         uses_envs=False,
-        call_non_memoizable_because='Getting an analysis unit cannot appear'
-                                    ' in a memoized context'
+        # TODO (S917-027): re-enable this protection or remove it once we
+        # moved forward on memoization soundness issues in Langkit.
+        call_non_memoizable_because=(
+            None and
+            'Getting an analysis unit cannot appear in a memoized context'
+        )
     )
     def get_unit(name=Symbol.array, kind=AnalysisUnitKind,
                  load_if_needed=Bool):
@@ -7431,7 +7435,6 @@ class Name(Expr):
     @langkit_property(
         return_type=AnalysisUnit, external=True, uses_entity_info=False,
         uses_envs=False,
-
         # TODO (S917-027): re-enable this protection or remove it once we
         # moved forward on memoization soundness issues in Langkit.
         call_non_memoizable_because=(

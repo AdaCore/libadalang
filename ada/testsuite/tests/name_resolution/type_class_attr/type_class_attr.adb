e152a6b6ae0652a0e9aa65f7a6dc44d94a743274
Raphaël AMIARD <raph.amiard@gmail.com>
2019-10-04 23:39:39 +0200

    SA04-043: Add Type_Class attribute

---
+++ ada/testsuite/tests/name_resolution/type_class_attr/type_class_attr.adb
@@ -0,0 +1,9 @@
+with System.Aux_DEC; use System.Aux_DEC;
+
+procedure Type_Class_Attr is
+   procedure Foo (A : Boolean) is null;
+   procedure Foo (A : Type_Class) is null;
+begin
+   Foo (Boolean'Type_Class);
+   pragma Test_Statement;
+end Type_Class_Attr;

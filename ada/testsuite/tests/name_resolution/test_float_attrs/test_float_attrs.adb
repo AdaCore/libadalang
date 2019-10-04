6352c9b84ed3b04172008ba34577cc29726a0b62
Raphaël AMIARD <raph.amiard@gmail.com>
2019-10-04 23:37:39 +0200

    SA04-043: Add float function attributes

---
+++ ada/testsuite/tests/name_resolution/test_float_attrs/test_float_attrs.adb
@@ -0,0 +1,12 @@
+procedure Test_Float_Attrs is
+   A : Float := Float'Floor (12.0);
+   pragma Test_Statement;
+
+   B : Float := Float'Rounding (A);
+   pragma Test_Statement;
+
+   C : Float := Float'Ceiling (B + 15.0);
+   pragma Test_Statement;
+begin
+   null;
+end Test_Float_Attrs;

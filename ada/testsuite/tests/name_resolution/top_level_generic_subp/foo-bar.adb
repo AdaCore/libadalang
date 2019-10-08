909910599fe892d0fa59d4d19585d6ffa3d3db9e
Romain Beguet <beguet@adacore.com>
2019-10-08 15:04:00 +0200

    SA22-010: Fix nameres of top-level generic child subprogram bodies.

---
+++ ada/testsuite/tests/name_resolution/top_level_generic_subp/foo-bar.adb
@@ -0,0 +1,6 @@
+procedure Foo.Bar (X : U) is
+   R : Integer := X.F;
+   pragma Test_Statement;
+begin
+   null;
+end Foo.Bar;
---
+++ ada/testsuite/tests/name_resolution/top_level_generic_subp/foo.ads
@@ -0,0 +1,7 @@
+package Foo is
+   type U is private;
+private
+   type U is record
+      F : Integer;
+   end record;
+end Foo;

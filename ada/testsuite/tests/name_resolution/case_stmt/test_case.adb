7d3e10e96cfc1b455a0fa09f0b5634ccd785f11f
Romain Beguet <beguet@adacore.com>
2019-11-08 17:35:39 +0100

    SB13-013: Fix nameres of case statements.

--- ada/testsuite/tests/name_resolution/case_stmt/test_case.adb
+++ ada/testsuite/tests/name_resolution/case_stmt/test_case.adb
@@ -3,6 +3,7 @@ with Ada.Text_IO; use Ada.Text_IO;
 procedure Test_Case is
    function Foo return Integer is (12);
    function Foo return Float is (12.0);
+   procedure Foo is null;
 begin
    case Foo is
       when 12 => Put_Line ("I KNEW IT");

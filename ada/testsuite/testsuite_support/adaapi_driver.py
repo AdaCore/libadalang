813d1582599a36146e94b6bc11616d41c8b86602
Pierre-Marie de Rodat <derodat@adacore.com>
2019-11-21 09:44:14 -0500

    AdaAPIDriver: add argv control and non-zero status code expectations
    TN: SB06-030

--- ada/testsuite/testsuite_support/adaapi_driver.py
+++ ada/testsuite/testsuite_support/adaapi_driver.py
@@ -1,7 +1,6 @@
 from __future__ import absolute_import, division, print_function
 
-from testsuite_support.base_driver import (BaseDriver, SetupError,
-                                           catch_test_errors)
+from testsuite_support.base_driver import BaseDriver, catch_test_errors
 
 
 class AdaAPIDriver(BaseDriver):
@@ -14,16 +13,10 @@ class AdaAPIDriver(BaseDriver):
     def tear_up(self):
         super(AdaAPIDriver, self).tear_up()
 
-        if 'main' not in self.test_env:
-            raise SetupError('Missing "main" key in test.yaml')
-        main = self.test_env['main']
-
-        if 'input_sources' not in self.test_env:
-            raise SetupError('Missing "input_sources" key in test.yaml')
-        self.input_sources = self.test_env['input_sources']
-
-        self.check_file(main)
-        self.check_file_list('"input_sources"', self.input_sources)
+        self.main = self.test_env['main']
+        self.input_sources = self.test_env.get('input_sources', [])
+        self.argv = self.test_env.get('argv', []) + self.input_sources
+        self.status_code = self.test_env.get('status_code', 0)
 
         with open(self.working_dir('gen.gpr'), 'w') as f:
             f.write('''
@@ -44,7 +37,7 @@ class AdaAPIDriver(BaseDriver):
                       ("-g", "-O0", "-gnata", "-gnatwa");
                 end Compiler;
             end Gen;
-            '''.format(main_source=main,
+            '''.format(main_source=self.main,
                        exec_name=self.test_program))
 
     @catch_test_errors
@@ -55,8 +48,9 @@ class AdaAPIDriver(BaseDriver):
         # use static ones.
         argv = ['gprbuild', '-Pgen'] + self.gpr_scenario_vars
         self.run_and_check(argv, append_output=False)
-        self.run_and_check([self.test_program] + self.input_sources,
-                           for_debug=True, memcheck=True)
+        self.run_and_check([self.test_program] + self.argv,
+                           for_debug=True, memcheck=True,
+                           status_code=self.status_code)
 
     #
     # Helpers

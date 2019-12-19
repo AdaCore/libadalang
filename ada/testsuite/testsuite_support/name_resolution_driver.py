c4e9ed6eae486de7b425ee83bdcdff5b28eb69b9
Pierre-Marie de Rodat <derodat@adacore.com>
2019-12-19 15:52:20 +0100

    Remove nameres.py
    Reaching feature parity with nameres.adb is very difficult and there is
    not much point in running both for all testcases.
    
    TN: SC13-001

--- ada/testsuite/testsuite_support/name_resolution_driver.py
+++ ada/testsuite/testsuite_support/name_resolution_driver.py
@@ -1,31 +1,14 @@
 from __future__ import absolute_import, division, print_function
 
-import difflib
-import os
-import os.path
-
 from testsuite_support.base_driver import (BaseDriver, SetupError,
                                            catch_test_errors)
-from testsuite_support.python_driver import PythonRunner
 
 
 class NameResolutionDriver(BaseDriver):
 
-    @property
-    def run_python(self):
-        return self.py_runner.is_python_api_available
-
-    @property
-    def run_python_only(self):
-        return self.test_env.get('run_python_only', False)
-
     @catch_test_errors
     def tear_up(self):
         super(NameResolutionDriver, self).tear_up()
-
-        self.py_runner = PythonRunner(self)
-        self.py_file = os.path.join(self.py_runner.support_dir,
-                                    'nameres.py')
 
         if 'input_sources' not in self.test_env:
             raise SetupError('Missing "input_sources" key in test.yaml')
@@ -38,9 +21,6 @@ class NameResolutionDriver(BaseDriver):
 
         self.imprecise_fallback = self.test_env.get('imprecise_fallback',
                                                     False)
-
-        if self.run_python:
-            self.py_runner.setup_environment()
 
     @catch_test_errors
     def run(self):
@@ -56,29 +36,4 @@ class NameResolutionDriver(BaseDriver):
         if self.imprecise_fallback:
             args.insert(0, '--imprecise-fallback')
 
-        # Depending on whether Python is available and whether we want to run
-        # only the Python driver, run both the Python and the Ada drivers for
-        # name resolution.
-        if self.run_python:
-            py_output = self.py_runner.run(self.py_file, args)
-            if self.run_python_only:
-                return
-
-        ada_output = self.run_and_check(
-            ['nameres'] + args,
-            for_debug=True, memcheck=True, append_output=not self.run_python
-        )
-        if not self.run_python:
-            return
-
-        diff = '\n'.join(difflib.unified_diff(py_output.splitlines(),
-                                              ada_output.splitlines(),
-                                              fromfile='python',
-                                              tofile='ada',
-                                              lineterm=''))
-        if diff:
-            with open(self.output_file, 'a') as f:
-                f.write('\n')
-                f.write('The Python driver and the Ada driver did not return'
-                        ' the same result:\n')
-                f.write(diff)
+        self.run_and_check(['nameres'] + args, for_debug=True, memcheck=True)

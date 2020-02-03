ac2914ec0782474e90203d47a2d7a2ccd97e5377
Pierre-Marie de Rodat <derodat@adacore.com>
2020-02-03 09:45:41 -0500

    name_resolution/storage_pool_attr: switch to UIDs for runtime entities
    TN: T203-031

--- ada/testsuite/tests/name_resolution/storage_pool_attr/storage_pool_attr.adb
+++ ada/testsuite/tests/name_resolution/storage_pool_attr/storage_pool_attr.adb
@@ -8,8 +8,8 @@ procedure Storage_Pool_Attr is
    Add : System.Address;
 
    S : System.Storage_Pools.Root_Storage_Pool'Class := B'Storage_Pool;
-   pragma Test_Statement;
+   pragma Test_Statement_UID;
 begin
    S.Allocate (Add, 12, 16);
-   pragma Test_Statement;
+   pragma Test_Statement_UID;
 end Storage_Pool_Attr;

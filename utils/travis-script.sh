0f78cee08a5981301cdac53f6543587e9bbf9057
Pierre-Marie de Rodat <derodat@adacore.com>
2020-02-05 04:43:10 -0500

    Travis CI: fix grep pattern to detect test failure

--- utils/travis-script.sh
+++ utils/travis-script.sh
@@ -41,7 +41,7 @@ then
     ada/manage.py test --disable-ocaml -- -j16 | tee TESTSUITE_OUT
 
     # Exit with an error if there is a FAILED line in TESTSUITE_OUT
-    ! grep "FAILED   " TESTSUITE_OUT > /dev/null
+    ! grep "FAILED" TESTSUITE_OUT > /dev/null
 fi
 
 # Install result and pack an archive

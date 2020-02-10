0218b4bec1ff3b1a67190fa06786c0648f8173b7
Pierre-Marie de Rodat <derodat@adacore.com>
2020-02-10 14:36:38 +0100

    Extend testing for Libadalang.Helpers.App
    TN: SC19-030

---
+++ ada/testsuite/tests/ada_api/app/project_vars/main.adb
@@ -0,0 +1,28 @@
+--  Check that App's -X argument (for scenario variables) works as expected
+
+with Ada.Text_IO; use Ada.Text_IO;
+
+with Libadalang.Analysis; use Libadalang.Analysis;
+with Libadalang.Helpers;  use Libadalang.Helpers;
+
+procedure Main is
+   procedure Process_Unit (Context : App_Job_Context; Unit : Analysis_Unit);
+
+   package App is new Libadalang.Helpers.App
+     (Name         => "example",
+      Description  => "Example app",
+      Process_Unit => Process_Unit);
+
+   ------------------
+   -- Process_Unit --
+   ------------------
+
+   procedure Process_Unit (Context : App_Job_Context; Unit : Analysis_Unit) is
+      pragma Unreferenced (Context);
+   begin
+      Put_Line (Unit.Root.Short_Image);
+   end Process_Unit;
+
+begin
+   App.Run;
+end Main;

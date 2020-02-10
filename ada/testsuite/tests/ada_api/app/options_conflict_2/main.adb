57e7ba8c473ea0d5a337450b98e1f7697e05bac3
Pierre-Marie de Rodat <derodat@adacore.com>
2020-02-10 13:44:04 +0100

    Add tests for invalid uses of Helpers.App
    TN: SC19-030

--- ada/testsuite/tests/ada_api/app/options_conflict_2/main.adb
+++ ada/testsuite/tests/ada_api/app/options_conflict_2/main.adb
@@ -2,8 +2,8 @@ with Libadalang.Helpers;
 
 procedure Main is
    package App is new Libadalang.Helpers.App
-     (Name        => "example",
-      Description => "Example app");
+     (Name         => "example",
+      Description  => "Example app.");
 begin
    App.Run;
 end Main;

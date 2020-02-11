d6bcb0b500ba1f78f25f109ec5dca84fdb335571
Pierre-Marie de Rodat <derodat@adacore.com>
2020-02-11 17:04:03 +0100

    Libadalang.Expr_Eval: protect against unexp. # of args in 'Succ/'Pred
    TN: T211-045

--- ada/testsuite/tests/properties/expr_eval/attr_succ_pred/invalid.ads
+++ ada/testsuite/tests/properties/expr_eval/attr_succ_pred/invalid.ads
@@ -6,4 +6,10 @@ package Invalid is
    Enum_Pred : constant Boolean := Boolean'Pred (False);
    --% node.f_default_expr.p_eval_as_int
 
+   No_Arg : constant Boolean := Boolean'Succ;
+   --% node.f_default_expr.p_eval_as_int
+
+   Two_Args : constant Boolean := Boolean'Succ (True, False);
+   --% node.f_default_expr.p_eval_as_int
+
 end Invalid;

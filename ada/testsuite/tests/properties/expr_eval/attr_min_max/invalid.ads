c6fa28e2887129825b36ce7792d3bf4c6737aabf
Pierre-Marie de Rodat <derodat@adacore.com>
2020-02-11 17:02:52 +0100

    Libadalang.Expr_Eval: protect against unexpected # of args in 'Min/'Max
    TN: T211-045

--- ada/testsuite/tests/properties/expr_eval/attr_min_max/invalid.ads
+++ ada/testsuite/tests/properties/expr_eval/attr_min_max/invalid.ads
@@ -3,6 +3,15 @@ package Invalid is
    Inconsistent_Type : constant Integer := Integer'Max (1, 1.0);
    --% node.f_default_expr.p_eval_as_int
 
+   No_Args : constant Integer := Integer'Max;
+   --% node.f_default_expr.p_eval_as_int
+
+   One_Arg : constant Integer := Integer'Max (1);
+   --% node.f_default_expr.p_eval_as_int
+
+   Three_Args : constant Integer := Integer'Max (1, 2, 3);
+   --% node.f_default_expr.p_eval_as_int
+
    For_Enum : constant Boolean := Boolean'Min (True, False);
    --% node.f_default_expr.p_eval_as_int
 

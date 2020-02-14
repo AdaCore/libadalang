01fa85f9b5397ac83d95fdbc2aa67239eaac5eb6
Romain Beguet <beguet@adacore.com>
2020-02-14 11:44:49 +0100

    T103-024: Fix rebindings being forgotten when resolving DerivedTypeDefs.

---
+++ ada/testsuite/tests/properties/expr_eval/with_rebindings_2/test.py
@@ -0,0 +1,23 @@
+from __future__ import absolute_import, division, print_function
+
+import libadalang as lal
+
+
+ctx = lal.AnalysisContext()
+u = ctx.get_from_file("test.adb")
+
+V_decl = u.root.findall(lal.ObjectDecl)[1]
+R_Arr_ref = V_decl.f_type_expr
+R_Arr_decl = R_Arr_ref.p_designated_type_decl
+R_ref = (R_Arr_decl.f_type_def.f_subtype_indication
+         .f_constraint.f_constraints[0].f_discr_expr)
+R_decl = R_ref.p_referenced_decl()
+R_range = R_decl.p_discrete_range
+
+print ("In instantiation {}:".format(R_Arr_decl.p_generic_instantiations[0]))
+print ("  R_Arr's index range is {} .. {}".format(
+    R_range.low_bound.p_eval_as_int,
+    R_range.high_bound.p_eval_as_int
+))
+
+print("Done")

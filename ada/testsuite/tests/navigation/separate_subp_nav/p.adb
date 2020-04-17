30219c3aef6abb1796a86a08fad464505bb94260
Raphaël AMIARD <raph.amiard@gmail.com>
2020-04-17 15:58:05 +0200

    T108-060: fix navigation for subprograms in separate packages

--- ada/testsuite/tests/navigation/separate_subp_nav/p.adb
+++ ada/testsuite/tests/navigation/separate_subp_nav/p.adb
@@ -2,4 +2,11 @@ package body P is
 
    procedure X (F : Integer) is separate;
 
+   package Nested is
+      procedure Boo;
+      --% $node.p_next_part_for_decl
+   end Nested;
+
+   package body Nested is separate;
+
 end P;

ae48eb03747ecf8f9b8acaebd8483ee775b5d84c
Romain Beguet <beguet@adacore.com>
2020-04-27 16:35:11 +0200

    T416-026: Fix p_is_static_decl for array types.

--- ada/language/ast.py
+++ ada/language/ast.py
@@ -5079,6 +5079,11 @@ class ArrayIndices(AdaNode):
     def index_type(dim=Int):
         pass
 
+    is_static = AbstractProperty(
+        type=Bool, dynamic_vars=[default_imprecise_fallback()],
+        doc="""Return True iff all index types are static."""
+    )
+
 
 class UnconstrainedArrayIndices(ArrayIndices):
     """
@@ -5100,6 +5105,10 @@ class UnconstrainedArrayIndices(ArrayIndices):
         return Entity.types.logic_all(
             lambda typ: typ.subtype_indication.sub_equation
         )
+
+    is_static = Property(Entity.types.all(
+        lambda t: t.subtype_indication.is_static_subtype
+    ))
 
 
 class ConstrainedArrayIndices(ArrayIndices):
@@ -5140,6 +5149,14 @@ class ConstrainedArrayIndices(ArrayIndices):
             lambda _: No(T.BaseTypeDecl.entity)
         )
 
+    is_static = Property(Entity.list.all(
+        lambda t: t.match(
+            lambda st=T.SubtypeIndication: st.is_static_subtype,
+            lambda e=T.BinOp: e.left.is_static_expr & e.right.is_static_expr,
+            lambda _: False
+        )
+    ))
+
 
 class ComponentDef(AdaNode):
     """
@@ -5178,6 +5195,8 @@ class ArrayTypeDef(TypeDef):
             Entity.indices.sub_equation,
             Entity.component_type.sub_equation
         )
+
+    is_static = Property(Entity.indices.is_static)
 
     xref_entry_point = Property(True)
 

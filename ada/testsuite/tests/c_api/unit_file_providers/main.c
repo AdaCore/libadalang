3c4f3f0f3029a082a0c809287f07f6d46508f3d3
Pierre-Marie de Rodat <derodat@adacore.com>
2020-04-29 11:31:47 +0200

    Revert "Update tests after recent Langkit changes for the C API"
    This reverts commit 957924859d6e500fb0f1a0eb212767167e7bd1ea, after
    followup changes in Langkit.
    
    TN: T423-042

--- ada/testsuite/tests/c_api/unit_file_providers/main.c
+++ ada/testsuite/tests/c_api/unit_file_providers/main.c
@@ -117,7 +117,7 @@ main(void)
     }
     if (entities->n == 0)
       printf("  <nothing>\n");
-    ada_ada_node_array_dec_ref(&entities);
+    ada_ada_node_array_dec_ref(entities);
 
     ada_context_decref(ctx);
     ada_dec_ref_unit_provider(ufp);

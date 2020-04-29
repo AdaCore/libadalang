4b3ed8daf6267d1419d855f466df266205381cbf
Romain Beguet <beguet@adacore.com>
2020-04-29 12:29:17 +0200

    T410-017: Fix crash of navigation on defining names.

---
+++ ada/testsuite/tests/navigation/abstract_subp/test.adb
@@ -0,0 +1,10 @@
+procedure Test is
+   package Pkg is
+      type T is tagged null record;
+
+      procedure Foo (Self : T) is abstract;
+      --% node.p_defining_name.p_next_part
+   end Pkg;
+begin
+
+end Test;

996640c2ec2eb6d94537c18498365b67a0f11623
Pierre-Marie de Rodat <derodat@adacore.com>
2020-05-14 15:35:45 +0200

    dsl_unparse: use local build directory
    TN: T511-017

--- ada/testsuite/tests/dsl_unparse/test.py
+++ ada/testsuite/tests/dsl_unparse/test.py
@@ -12,7 +12,16 @@ unparse_dest = os.path.abspath('lal.lkt')
 unparse_script = 'to:{},lexer,grammar,nodes'.format(unparse_dest)
 
 status = subprocess.call(
-    [sys.executable, os.path.join('ada', 'manage.py'), '-v=none', '-E',
+    [sys.executable, os.path.join('ada', 'manage.py'),
+     '-v=none', '-E',
+
+     # The call to "generate" will generate not only the concrete DSL, but also
+     # the Ada sources. Target a build directory that is local to this
+     # testcase's working directory to avoid messing with the sources of the
+     # library we are currently testing, as this could trigger parallel
+     # compilations of Libadalang, leading to obscure failures.
+     '--build-dir', os.path.abspath('build'),
+
      'generate', '-P', '--unparse-script', unparse_script],
     cwd=os.environ['LIBADALANG_ROOTDIR']
 )

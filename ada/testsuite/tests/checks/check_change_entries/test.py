4517f81c3da38d6d6a03586abdf66784b7cf233b
Pierre-Marie de Rodat <derodat@adacore.com>
2020-05-18 12:27:49 +0200

    check_change_entries: fix on Windows

--- ada/testsuite/tests/checks/check_change_entries/test.py
+++ ada/testsuite/tests/checks/check_change_entries/test.py
@@ -2,10 +2,13 @@ from __future__ import absolute_import, division, print_function
 
 import os
 import subprocess
+import sys
+
 from utils import LAL_ROOTDIR
 
 
 subprocess.check_call([
+    sys.executable,
     os.path.join(LAL_ROOTDIR, 'user_manual', 'changes', 'process_changes.py'),
     'rst',
     '--quiet'

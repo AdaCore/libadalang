00bf3cf467932d5925ff4e61fa95a0c632cc556c
Maxim Reznik <reznikmm@gmail.com>
2020-05-27 13:27:26 +0300

    Update Travis CI scripts to use GNAT Community 2020

--- utils/travis-install.sh
+++ utils/travis-install.sh
@@ -6,30 +6,29 @@
 set -v
 set -e
 
-if ! [ -d $TOOLS_DIR ]
-then
+if ! [ -d $TOOLS_DIR ]; then
     mkdir -p $TOOLS_DIR
 fi
-if ! [ -d $INSTALL_DIR ]
-then
+if [ -f $INSTALL_DIR/bin/*gcc-8.3.1 ]; then
+    rm -rf $INSTALL_DIR # Delete GNAT Community 2019
+fi
+if ! [ -d $INSTALL_DIR ]; then
     mkdir -p $INSTALL_DIR
 fi
 
 # Get and install GNAT
-if ! [ -d gnat_community_install_script ]
-then
+if ! [ -d gnat_community_install_script ]; then
     git clone https://github.com/AdaCore/gnat_community_install_script.git
 else
     (cd gnat_community_install_script && git pull)
 fi
-if ! [ -f $INSTALL_DIR/bin/gcc ]
-then
+if ! [ -f $INSTALL_DIR/bin/gcc ]; then
     if [ $TRAVIS_OS_NAME = linux ]; then
-        GNAT_INSTALLER=$TOOLS_DIR/gnat-community-2019-20190517-x86_64-linux-bin
-        GNAT_INSTALLER_URL="https://community.download.adacore.com/v1/0cd3e2a668332613b522d9612ffa27ef3eb0815b?filename=gnat-community-2019-20190517-x86_64-linux-bin"
+        GNAT_INSTALLER=$TOOLS_DIR/gnat-2020-20200429-x86_64-linux-bin
+        GNAT_INSTALLER_URL="https://community.download.adacore.com/v1/4d99b7b2f212c8efdab2ba8ede474bb9fa15888d?filename=gnat-2020-20200429-x86_64-linux-bin"
     else
-        GNAT_INSTALLER=$TOOLS_DIR/gnat-community-2019-20190517-x86_64-darwin-bin.dmg
-        GNAT_INSTALLER_URL="https://community.download.adacore.com/v1/5a7801fc686e86de838cfaf7071170152d81254d?filename=gnat-community-2019-20190517-x86_64-darwin-bin.dmg"
+        GNAT_INSTALLER=$TOOLS_DIR/gnat-2020-20200429-x86_64-darwin-bin.dmg
+        GNAT_INSTALLER_URL="https://community.download.adacore.com/v1/4470dd195aec672d7c2f2a2bac3dcf6e59bbb26c?filename=gnat-2020-20200429-x86_64-darwin-bin.dmg"
     fi
 
     wget -O $GNAT_INSTALLER $GNAT_INSTALLER_URL
@@ -39,22 +38,19 @@ then
 fi
 
 # Get gprbuild (to build libgpr)
-if [ -d "$TOOLS_DIR/gprbuild" ]
-then
+if [ -d "$TOOLS_DIR/gprbuild" ]; then
     (cd $TOOLS_DIR/gprbuild && git pull)
 else
     (cd $TOOLS_DIR && git clone https://github.com/AdaCore/gprbuild)
 fi
 
 # Get gnatcoll-core and gnatcoll-bindings
-if [ -d "$TOOLS_DIR/gnatcoll-core" ]
-then
+if [ -d "$TOOLS_DIR/gnatcoll-core" ]; then
     (cd $TOOLS_DIR/gnatcoll-core && git pull)
 else
     (cd $TOOLS_DIR && git clone https://github.com/AdaCore/gnatcoll-core)
 fi
-if [ -d "$TOOLS_DIR/gnatcoll-bindings" ]
-then
+if [ -d "$TOOLS_DIR/gnatcoll-bindings" ]; then
     (cd $TOOLS_DIR/gnatcoll-bindings && git pull)
 else
     (cd $TOOLS_DIR && git clone https://github.com/AdaCore/gnatcoll-bindings)
@@ -62,8 +58,7 @@ fi
 
 # Get Langkit, in particular the branch that corresponds to the Libadalang
 # commit being tested.
-if ! [ -d langkit ]
-then
+if ! [ -d langkit ]; then
     git clone https://github.com/AdaCore/langkit
 fi
 (
@@ -115,7 +110,6 @@ gcc -v
 
 # Make sure libpythonlang is installed for platforms that will
 # run the testsuite, as its a test dependency.
-if [ "$TRAVIS_OS_NAME" != osx ]
-then
+if [ "$TRAVIS_OS_NAME" != osx ]; then
     langkit/scripts/manage.sh make
 fi

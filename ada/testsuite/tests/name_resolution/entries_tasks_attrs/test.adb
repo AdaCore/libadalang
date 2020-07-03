37e7a9fc27aefe9bc4401e09e3a771355493a859
Romain Beguet <romain-be@hotmail.com>
2020-07-03 16:55:08 +0200

    T703-010: Name resolution for task and entry attributes.

--- ada/testsuite/tests/name_resolution/entries_tasks_attrs/test.adb
+++ ada/testsuite/tests/name_resolution/entries_tasks_attrs/test.adb
@@ -1,15 +1,20 @@
 procedure Test is
    procedure Foo (A : Integer) is null;
    procedure Foo (A : Boolean) is null;
-   task type T;
+   task type T is
+      entry E;
+   end T;
 
    task body T is
    begin
-      null;
+      Foo (E'Count);
+      pragma Test_Statement;
    end T;
 
    Inst : T;
 begin
    Foo (Inst'Callable);
+   pragma Test_Statement;
+   Foo (Inst'Terminated);
    pragma Test_Statement;
 end Test;

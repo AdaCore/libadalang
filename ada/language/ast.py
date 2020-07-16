4b93622e80bfd39fad2cd734447010149a593938
Daniel Mercier <mercier@adacore.com>
2020-07-16 13:42:50 +0200

    T710-008: Add memoization to previous_part on a Body

--- ada/language/ast.py
+++ ada/language/ast.py
@@ -2410,7 +2410,7 @@ class Body(BasicDecl):
             Entity.unbound_previous_part.then(lambda d: d.node.children_env)
         )
 
-    @langkit_property(public=True, return_type=T.BasicDecl.entity)
+    @langkit_property(public=True, return_type=T.BasicDecl.entity, memoized=True)
     def previous_part():
         """
         Return the previous part for this body. Might be a declaration or a

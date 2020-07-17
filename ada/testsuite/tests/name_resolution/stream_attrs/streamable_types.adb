350c296e57a2322f3a63b634b970a580e51c7ce6
Romain Beguet <beguet@adacore.com>
2020-07-17 12:54:30 +0200

    T716-015: Add xref for stream attributes references.

---
+++ ada/testsuite/tests/name_resolution/stream_attrs/streamable_types.adb
@@ -0,0 +1,13 @@
+with Ada.Streams; use Ada.Streams;
+with Ada.Streams.Stream_IO;
+
+package body Streamable_Types is
+   procedure Main is
+      X : Int;
+      F : Ada.Streams.Stream_IO.File_Type;
+      S : Ada.Streams.Stream_IO.Stream_Access;
+   begin
+      Int'Output (S, X);
+      pragma Test_Statement;
+   end Main;
+end Streamable_Types;

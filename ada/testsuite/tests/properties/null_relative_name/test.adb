a1d0900a5f71ab1973e4a92b0f3a97dcd57d22b3
Pierre-Marie de Rodat <derodat@adacore.com>
2020-09-15 12:01:09 +0200

    properties/null_relative_name: fix testcase to work on Python2
    TN: T907-010

--- ada/testsuite/tests/properties/null_relative_name/test.adb
+++ ada/testsuite/tests/properties/null_relative_name/test.adb
@@ -2,6 +2,13 @@ procedure Test is
 begin
    null;
 exception
-   when A => null;
-   when B : C => null;
+   when A =>
+      --% $node.p_relative_name
+      --% $node.p_relative_name_text
+      null;
+
+   when B : C =>
+      --% $node.p_relative_name
+      --% $node.p_relative_name_text
+      null;
 end Test;

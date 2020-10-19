f879a4f9c749dd7453b5719fbef40dc034ed9fce
Romain Beguet <beguet@adacore.com>
2020-10-19 15:22:28 +0200

    TA05-019: Turn test case into valid Ada.

--- testsuite/tests/name_resolution/xref_eq_tagged_primitives/tagged_prims.adb
+++ testsuite/tests/name_resolution/xref_eq_tagged_primitives/tagged_prims.adb
@@ -2,17 +2,21 @@ procedure Tagged_Prims is
 
    type Int is range 0 .. 102;
 
-   type T is tagged record
-      Baz : Int;
-   end record;
+   package Pkg is
+      type T is tagged record
+         Baz : Int;
+      end record;
 
-   procedure Foo (Self : T) is null;
-   procedure Bar (Self : access T) is null;
+      procedure Foo (Self : T) is null;
+      procedure Bar (Self : access T) is null;
 
-   procedure Baz (Self : T'Class) is null;
-   procedure Qux (Self : access T'Class) is null;
+      procedure Baz (Self : T'Class) is null;
+      procedure Qux (Self : access T'Class) is null;
 
-   procedure Baz (Self : T'Class; Pouet : Int) is null;
+      procedure Baz (Self : T'Class; Pouet : Int) is null;
+   end Pkg;
+
+   use Pkg;
 
    T_Inst : T;
 

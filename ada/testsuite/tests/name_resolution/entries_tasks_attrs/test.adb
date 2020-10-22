c6bcc8614327009b3e83cd66504bba4d46e951ac
Romain Beguet <beguet@adacore.com>
2020-10-22 16:39:49 +0200

    T907-012: Add support for Caller attribute.

--- ada/testsuite/tests/name_resolution/entries_tasks_attrs/test.adb
+++ ada/testsuite/tests/name_resolution/entries_tasks_attrs/test.adb
@@ -1,3 +1,5 @@
+with Ada.Task_Identification;
+
 procedure Test is
    procedure Foo (A : Integer) is null;
    procedure Foo (A : Boolean) is null;
@@ -9,6 +11,16 @@ procedure Test is
    begin
       Foo (E'Count);
       pragma Test_Statement;
+
+      accept E do
+         declare
+            I : Ada.Task_Identification.Task_Id :=
+               E'Caller;
+            pragma Test_Statement;
+         begin
+            null;
+         end;
+      end E;
    end T;
 
    Inst : T;

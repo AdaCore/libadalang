f6e23b33ae2b889ac72c64e1316f59582019bb79
Romain Beguet <beguet@adacore.com>
2020-10-30 17:03:14 +0100

    TA30-018: Remove erroneous assumption in candidate_type_for_primitive.

--- testsuite/tests/properties/is_dispatching_call_2/test.adb
+++ testsuite/tests/properties/is_dispatching_call_2/test.adb
@@ -1,3 +1,5 @@
+with Interfaces.C.Strings;
+
 procedure Test is
    generic
       type T is private;
@@ -8,6 +10,11 @@ procedure Test is
       Foo (X);
       --% node.f_call.p_is_dispatching_call()
    end Foo;
+
+   type F_Access is access function return Interfaces.C.Strings.chars_ptr;
+   F : F_Access;
+   Str : String := Interfaces.C.Strings.Value (F.all);
+   --% node.f_default_expr.p_is_dispatching_call()
 begin
    null;
 end Test;

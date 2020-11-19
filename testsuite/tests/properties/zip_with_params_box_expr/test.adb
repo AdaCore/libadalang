125f2a2c7a436eb35281f95529657a443ef57c49
Romain Beguet <beguet@adacore.com>
2020-11-19 14:24:14 +0100

    T717-018: Improve zip_with_params with formal subprograms.

---
+++ testsuite/tests/properties/zip_with_params_box_expr/test.adb
@@ -0,0 +1,54 @@
+procedure Test is
+   package Pkg_Foo is
+      generic
+         with function F (X : Integer) return Integer is <>;
+      procedure Foo;
+
+      procedure Foo is null;
+   end Pkg_Foo;
+
+   package Pkg_Bar is
+      generic
+         type T is private;
+         with function F (X : T) return T is <>;
+      procedure Bar;
+
+      procedure Bar is null;
+   end Pkg_Bar;
+
+   package Basic is
+      function F (X : Integer) return Integer is (X);
+      function F (X : Float) return Float is (X);
+
+      procedure My_Foo is new Pkg_Foo.Foo;
+      --% node.f_params.p_zip_with_params()
+
+      procedure My_Int_Bar is new Pkg_Bar.Bar (T => Integer);
+      --% node.f_params.p_zip_with_params()
+
+      procedure My_Float_Bar is new Pkg_Bar.Bar (T => Float);
+      --% node.f_params.p_zip_with_params()
+   end Basic;
+
+   package Generic_Actual is
+      generic
+         type T is private;
+      function Make_F (X : T) return T;
+
+      function Make_F (X : T) return T is (X);
+
+      function F is new Make_F (T => Float);
+
+      procedure My_Float_Bar is new Pkg_Bar.Bar (T => Float);
+      --% node.f_params.p_zip_with_params()
+   end Generic_Actual;
+
+   package Explicit is
+      function G (X : Integer) return Integer is (X);
+
+      procedure My_Foo is new Pkg_Foo.Foo (F => G);
+      --% node.f_params.p_zip_with_params()
+   end Expicit;
+begin
+   null;
+end Test;

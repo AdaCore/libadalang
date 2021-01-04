03cb521392f1bc7e55379eab4a798841a72e1720
Romain Beguet <beguet@adacore.com>
2021-01-04 16:50:36 +0100

    U104-020: Fix null dereference in decl_spec.

--- testsuite/tests/properties/primitive_subp_tagged_type/foo.adb
+++ testsuite/tests/properties/primitive_subp_tagged_type/foo.adb
@@ -1,4 +1,7 @@
 package body Foo is
    procedure Bar (Self : T) is null;
    --% $node.p_subp_spec_or_null().p_primitive_subp_tagged_type()
+
+   type Test_Access is access procedure (Self : T);
+   --% $node.f_type_def.f_subp_spec.p_primitive_subp_tagged_type()
 end Foo;

f04445a0cafbbc0bbad63fa8da8f484b95804fa1
Pierre-Marie de Rodat <derodat@adacore.com>
2021-01-14 09:32:56 -0500

    Add the error_node annotation after recent Langkit changes
    TN: T520-011

--- ada/ast.py
+++ ada/ast.py
@@ -2222,6 +2222,7 @@ class ErrorDecl(BasicDecl):
     """
     Placeholder node for syntax errors in lists of declarations.
     """
+    error_node = True
     aspects = NullField()
     defining_names = Property(No(T.DefiningName.entity.array))
 
@@ -13759,8 +13760,7 @@ class ErrorStmt(Stmt):
     """
     Placeholder node for syntax errors in lists of statements.
     """
-
-    pass
+    error_node = True
 
 
 @abstract

4b361b6ccd8c7cb7d24008f1a650988d1310ff10
Romain Beguet <beguet@adacore.com>
2021-03-23 11:36:45 +0100

    U322-044: Never return a Precise result with a null node.

--- testsuite/tests/properties/failsafe_referenced_def_name/test.adb
+++ testsuite/tests/properties/failsafe_referenced_def_name/test.adb
@@ -20,6 +20,10 @@ procedure Test is
    E : Integer :=
       F  --% node.p_failsafe_referenced_def_name()
       ;
+
+   G : Integer := D
+      'Size  --% node.p_failsafe_referenced_def_name()
+      ;
 begin
    null;
 end Test;

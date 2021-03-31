48b0d71c6ce96ae1b164681389caa32d17546be1
Romain Beguet <beguet@adacore.com>
2021-03-31 17:56:57 +0200

    U331-032: Improve get/has_aspect for Inline aspect.
    It will now check/find the aspect on any part of the given entity.

---
+++ testsuite/tests/properties/has_aspect_2/test.adb
@@ -0,0 +1,30 @@
+procedure Test is
+   A : constant Boolean := True;
+   B : constant Boolean := False;
+
+   procedure Pouet0 is null;
+   --% $node.p_has_aspect('inline')
+
+   procedure Pouet1 is null
+      with Inline;
+   --% $node.p_has_aspect('inline')
+
+   procedure Pouet2
+      with Inline;
+
+   procedure Pouet2 is null;
+   --% $node.p_has_aspect('inline')
+
+   package Pkg is
+      procedure Pouet3;
+      --% $node.p_has_aspect('inline')
+   end Pkg;
+
+   package body Pkg is
+      procedure Pouet3 is null;
+
+      pragma Inline (Pouet3);
+   end Pkg;
+begin
+   null;
+end Test;

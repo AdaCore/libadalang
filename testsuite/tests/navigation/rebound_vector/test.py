45e3fda51a7892bfc52d1327388ef1ef34da5aad
Romain Beguet <beguet@adacore.com>
2021-07-01 15:33:26 +0200

    U630-018: Prevent crash when computing ref'd env of forward use clause.
    This crash was uncovered by TB18-010.

---
+++ testsuite/tests/navigation/rebound_vector/test.py
@@ -0,0 +1,10 @@
+import libadalang as lal
+
+ctx = lal.AnalysisContext()
+u = ctx.get_from_file("test.ads")
+td = u.root.find(lal.TypeDecl)
+base_name = td.f_type_def.f_subtype_indication.f_name
+vec = base_name.p_referenced_decl()
+noop = vec.p_next_part.p_previous_part()
+assert (noop is not None)
+print("Done.")

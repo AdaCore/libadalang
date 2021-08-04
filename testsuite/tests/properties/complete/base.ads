06271290d328295cce6ab9134fceacaa4e10a52e
Raphaël AMIARD <raph.amiard@gmail.com>
2021-08-04 18:56:37 +0200

    U802-015 completion: Dottable subp are always visible

---
+++ testsuite/tests/properties/complete/base.ads
@@ -0,0 +1,4 @@
+package Base is
+   type Base_Type is tagged null record;
+   procedure Base_Method (Self : Base_Type) is null;
+end Base;
--- testsuite/tests/properties/complete/test_pkg.ads
+++ testsuite/tests/properties/complete/test_pkg.ads
@@ -1,5 +1,7 @@
+with Base;
+
 package Test_Pkg is
-   type A is tagged null record;
+   type A is new Base.Base_Type with null record;
 
    function My_Method (Self : A) return Integer;
 end Test_Pkg;

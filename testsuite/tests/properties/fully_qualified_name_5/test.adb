d9ad307383e941bdd2d42aec3621ea100c6fa5f6
Raphaël AMIARD <raph.amiard@gmail.com>
2021-08-10 13:58:00 +0200

    T730-025, U430-027: Add DefiningName.fully_qualified_name
    Make BasicDecl.fully_qualified_name raise an exception when the basic
    declaration has several names.

---
+++ testsuite/tests/properties/fully_qualified_name_5/test.adb
@@ -0,0 +1,11 @@
+with Ada.Unchecked_Deallocation;
+
+procedure Test is
+   A, B : Integer;
+   --% node.p_fully_qualified_name
+   --% node.p_defining_names[0].p_fully_qualified_name
+   --% node.p_defining_names[1].p_fully_qualified_name
+   --% node.p_defining_names[1].p_canonical_fully_qualified_name
+begin
+   null;
+end Test;

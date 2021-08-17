d12d8ff2db07d02a89c18f83e0b74c4dd5df8e1d
Romain Beguet <beguet@adacore.com>
2021-08-17 15:19:07 +0200

    Minor: make test case compile.

--- testsuite/tests/properties/get_aspect_4/test.adb
+++ testsuite/tests/properties/get_aspect_4/test.adb
@@ -1,9 +1,14 @@
 procedure Test is
-   type R is record
-      X : Integer;
+   type R (B : Boolean) is record
+      case B is
+         when False =>
+            X : Integer;
+         when True =>
+            null;
+      end case;
    end record;
    --% node.p_get_aspect("unchecked_union")
    pragma Unchecked_Union (R);
 begin
-
+   null;
 end Test;

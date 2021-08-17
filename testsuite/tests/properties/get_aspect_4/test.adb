3b4d1b023f829f1a4f690d9b24e36bc6477ed9da
Romain Beguet <beguet@adacore.com>
2021-08-17 15:19:46 +0200

    U803-008: Add support for more pragmas.

--- testsuite/tests/properties/get_aspect_4/test.adb
+++ testsuite/tests/properties/get_aspect_4/test.adb
@@ -9,6 +9,16 @@ procedure Test is
    end record;
    --% node.p_get_aspect("unchecked_union")
    pragma Unchecked_Union (R);
+
+   type Arr is array (1 .. 10) of Integer;
+   --% node.p_get_aspect("volatile_components")
+   --% node.p_get_aspect("independent_components")
+   pragma Volatile_Components (Arr);
+   pragma Independent_Components (Arr);
+
+   type Enum is (A, B, C);
+   --% node.p_get_aspect("discard_names")
+   pragma Discard_Names (Enum);
 begin
    null;
 end Test;

edb5dab27c981ea18b0e1478208afbfc52011fc1
Pierre-Marie de Rodat <derodat@adacore.com>
2022-03-28 11:34:19 +0200

    Remove mistakenly added sources
    TN: V117-037

--- user_manual/foo/src/os.ads
+++
@@ -1,9 +0,0 @@
-package OS is
-   # if OS = "linux" then
-      Name : constant String := "gnu-linux";
-   # elsif OS = "windows" then
-      Name : constant String := "microsoft-windows";
-   # else
-      Name : constant String := "unknown";
-   # end if;
-end OS;

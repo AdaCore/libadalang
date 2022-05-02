7f64ae43a9bb20b533c2c80ff61110669861f2e6
Raphaël AMIARD <raph.amiard@gmail.com>
2022-05-02 15:01:28 +0200

    Don't include sources in generated/ directly
    So we don't generate the documentation twice, since they're already
    textually included.
    
    GL issue #923

--- user_manual/conf.py
+++ user_manual/conf.py
@@ -81,7 +81,7 @@ if libadalang.version:
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
-exclude_patterns = ['_build']
+exclude_patterns = ['_build', 'generated']
 
 # The reST default role (used for this markup: `text`) to use for all
 # documents.

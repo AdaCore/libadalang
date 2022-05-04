c5eac2c6c8cb1be6c61de6b65627002a419571b6
Pierre-Marie de Rodat <derodat@adacore.com>
2022-05-04 10:03:13 +0200

    AdaAPIDriver: remove obsolete comments, add new ones
    TN: U624-007

--- testsuite/drivers/adaapi_driver.py
+++ testsuite/drivers/adaapi_driver.py
@@ -2,6 +2,35 @@ from drivers.base_driver import BaseDriver
 
 
 class AdaAPIDriver(BaseDriver):
+    """
+    Driver to test the Ada API building and running an Ada test program.
+
+    "test.yaml" entries:
+
+    ``main``
+
+        Mandatory, name of the Ada source that is the main of the test program.
+
+    ``args``
+
+        Optional list of command-line arguments to pass to the test program.
+        The default is an empty list.
+
+    ``input_sources``
+
+        Optional list of source files, to be passed as additional command-line
+        arguments to the test program. The default is an empty list.
+
+    ``status_code``
+
+        Optional status code (int) that is expected for the test program
+        execution. The default is 0 (execution successful).
+
+    ``check_output``
+
+        Optional boolean: whether to include the test program output to the
+        test output (for baseline comparison). The default is True.
+    """
 
     @property
     def test_program(self):
@@ -10,9 +39,12 @@ class AdaAPIDriver(BaseDriver):
 
     def run(self):
         main = self.test_env['main']
+
         input_sources = self.test_env.get('input_sources', [])
         argv = self.test_env.get('argv', []) + input_sources
+
         status_code = self.test_env.get('status_code', 0)
+
         append_output = self.test_env.get('check_output', True)
 
         with open(self.working_dir('gen.gpr'), 'w') as f:
@@ -36,10 +68,7 @@ class AdaAPIDriver(BaseDriver):
             end Gen;
             '''.format(main_source=main, exec_name=self.test_program))
 
-        # Build the test program and then run it. Whether we use static or
-        # shared libraries, make it explicit: some dependencies (such as
-        # GNATcoll) use shared ones by default while others such as gnat_util
-        # use static ones.
+        # Build the test program and then run it
         self.run_and_check(['gprbuild', '-Pgen'] + self.gpr_scenario_vars,
                            append_output=False)
         self.run_and_check([self.test_program] + argv,

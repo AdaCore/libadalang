778cf9fa43565aaa7cb8d7eb7f7213ca81eef27b
Pierre-Marie de Rodat <derodat@adacore.com>
2022-05-04 10:29:48 +0200

    AdaAPIDriver: make it possible to run multiple test programs
    TN: U624-007

--- testsuite/drivers/adaapi_driver.py
+++ testsuite/drivers/adaapi_driver.py
@@ -13,27 +13,35 @@ class AdaAPIDriver(BaseDriver):
 
     ``args``
 
-        Optional list of command-line arguments to pass to the test program.
+        Optional list of command-line arguments to pass to the test programs.
         The default is an empty list.
 
     ``status_code``
 
-        Optional status code (int) that is expected for the test program
+        Optional status code (int) that is expected for the test programs
         execution. The default is 0 (execution successful).
 
     ``check_output``
 
-        Optional boolean: whether to include the test program output to the
+        Optional boolean: whether to include the test programs output to the
         test output (for baseline comparison). The default is True.
     """
 
-    @property
-    def test_program(self):
-        """Return the absolute path to the program to run for this testcase."""
-        return self.working_dir(self.test_env['test_name'])
-
     def run(self):
-        main = self.test_env['main']
+        # Get the list of mains
+        mains = self.test_env['main']
+        if isinstance(mains, str):
+            mains = [mains]
+
+        # Compute the list of test programs to run
+        test_programs = []
+        for m in mains:
+            ext = ".adb"
+            if not m.endswith(ext):
+                raise TestError(f"Invalid Ada source name: {m}")
+            exe_name = m[:-len(ext)] + self.env.build.os.exeext
+            test_programs.append(self.working_dir(exe_name))
+
         argv = self.test_env.get('argv', [])
 
         status_code = self.test_env.get('status_code', 0)
@@ -41,30 +49,39 @@ class AdaAPIDriver(BaseDriver):
         append_output = self.test_env.get('check_output', True)
 
         with open(self.working_dir('gen.gpr'), 'w') as f:
-            f.write('''
+            f.write(f"""
             with "libadalang";
 
             project Gen is
                 for Languages use ("Ada");
                 for Source_Dirs use (".");
                 for Object_Dir use ".";
-                for Main use ("{main_source}");
-
-                package Builder is
-                    for Executable ("{main_source}") use "{exec_name}";
-                end Builder;
+                for Main use ({', '.join(f'"{m}"' for m in mains)});
 
                 package Compiler is
                     for Default_Switches ("Ada") use
                       ("-g", "-O0", "-gnata", "-gnatwa");
                 end Compiler;
             end Gen;
-            '''.format(main_source=main, exec_name=self.test_program))
+            """)
 
-        # Build the test program and then run it
-        self.run_and_check(['gprbuild', '-Pgen'] + self.gpr_scenario_vars,
-                           append_output=False)
-        self.run_and_check([self.test_program] + argv,
-                           memcheck=True,
-                           status_code=status_code,
-                           append_output=append_output)
+        # Build the test programs and then run them
+        self.run_and_check(
+            ['gprbuild', '-Pgen'] + self.gpr_scenario_vars,
+            append_output=False,
+        )
+        for i, (m, tp) in enumerate(zip(mains, test_programs)):
+
+            # If there are multiple test programs to run, print a separator to
+            # distringuish their outputs in the baseline.
+            if i > 0:
+                self.output += "\n\n"
+            if len(test_programs) > 1:
+                self.output += f"== {m} ==\n\n"
+
+            self.run_and_check(
+                [tp] + argv,
+                memcheck=True,
+                status_code=status_code,
+                append_output=append_output,
+            )

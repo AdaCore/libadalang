265b98349097f1795ebc2c7e9016fcaac4c25f73
Pierre-Marie de Rodat <derodat@adacore.com>
2022-11-16 13:06:09 +0000

    Python API: make App warn about missing source files
    TN: VA12-016
    (cherry picked from commit 9738808ec4c7b1c283af2c7af4fdd41245906a79)

---
+++ testsuite/tests/python/app_missing_files/no_missing.adb
@@ -0,0 +1,3 @@
+procedure No_Missing is
+begin
+end No_Missing;
---
+++ testsuite/tests/python/app_missing_files/test.py
@@ -0,0 +1,55 @@
+"""
+Check that App reports missing source files as expected.
+"""
+
+import dataclasses
+import os.path
+import subprocess
+import sys
+from typing import List
+
+import libadalang as lal
+
+
+@dataclasses.dataclass
+class Testcase:
+    """
+    Each testcase runs the ``App`` below and passes it the command line
+    arguments ``args``.
+    """
+    label: str
+    args: List[str]
+
+
+class App(lal.App):
+    def process_unit(self, unit):
+        print("Processing:", os.path.basename(unit.filename))
+        unit.populate_lexical_env()
+
+
+tests = [
+    Testcase("no_missing", ["no_missing.adb", "no_such_file.adb"]),
+    Testcase(
+        "missing_warn",
+        ["no_missing.adb", "missing.adb", "no_such_file.adb"],
+    ),
+    Testcase(
+        "missing_error",
+        ["-E", "no_missing.adb", "missing.adb", "no_such_file.adb"],
+    ),
+]
+
+
+if len(sys.argv) == 1:
+    print("test.py: Starting...")
+    print("")
+    for t in tests:
+        print(f"== {t.label} ==")
+        print("")
+        sys.stdout.flush()
+        p = subprocess.run([sys.executable, __file__] + t.args)
+        print("Return code:", p.returncode)
+        print("")
+    print("test.py: Done.")
+else:
+    App.run(sys.argv[1:])

bef47580b5ff2b5b7dd60c1bd49e610b99843400
Pierre-Marie de Rodat <derodat@adacore.com>
2023-05-10 08:44:51 +0000

    Libadalang.Project_Provider: call GPR2.Project.Tree.Update_Sources
    Calling this procedure is necessary in order to get the unit-to-source
    file lookups from LibGPR2 working as expected. Instead of requiring
    Libadalang users to do it themselves (or potentially forget and get
    resolution errors), do it automatically in Libadalang.

--- extensions/src/libadalang-project_provider.adb
+++ extensions/src/libadalang-project_provider.adb
@@ -931,10 +931,18 @@ package body Libadalang.Project_Provider is
      (Tree : GPR2.Project.Tree.Object)
       return GPR2_Provider_And_Projects_Array_Access
    is
-      Result : Any_Provider_And_Projects_Array_Access :=
+      Result : Any_Provider_And_Projects_Array_Access;
+   begin
+      --  TODO??? (eng/gpr/gpr-issues#34) The implicit discovery of project
+      --  sources in GPR2.Project.View.Unit is broken: manually register
+      --  sources so that unit->source lookups work as expected.
+
+      Tree.Update_Sources (With_Runtime => True);
+
+      Result :=
         Create_Project_Unit_Providers
           ((Kind => GPR2_Kind, GPR2_Value => Tree.Reference));
-   begin
+
       --  Convert Result (GPR library agnostic data structure) into the return
       --  type (GPR2-specific data structure).
 
@@ -969,6 +977,12 @@ package body Libadalang.Project_Provider is
    is
       Dummy : Project_Unit_Provider_Access;
    begin
+      --  TODO??? (eng/gpr/gpr-issues#34) The implicit discovery of project
+      --  sources in GPR2.Project.View.Unit is broken: manually register
+      --  sources so that unit->source lookups work as expected.
+
+      Tree.Update_Sources (With_Runtime => True);
+
       return Result : LAL.Unit_Provider_Reference do
          Create_Project_Unit_Provider
            (Tree         => (Kind => GPR2_Kind, GPR2_Value => Tree.Reference),

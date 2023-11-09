89ae4cb9324a0870d08e3f12473fbf8c131bd95d
Laurent Thévenoux <thevenoux@adacore.com>
2023-11-09 14:30:28 +0100

    Support Result attribute on access-to-function type
    As stated in ARM 6.1.1 28/5, the Result attribute can be specified to
    an access-to-function type too (in addition to function declaration).

--- testsuite/tests/name_resolution/post_result/test_result_attr.adb
+++ testsuite/tests/name_resolution/post_result/test_result_attr.adb
@@ -6,6 +6,15 @@ procedure Test_Result_Attr is
    begin
       return 12;
    end Barize;
+
+   type T1 is access function (A : Boolean) return Boolean with
+      Post => T1'Result = A;
+
+   type T2 is access function (A : Boolean) return Boolean;
+
+   pragma Post (T2'Result = True);
+
 begin
+   null;
 end Test_Result_Attr;
 pragma Test_Block;

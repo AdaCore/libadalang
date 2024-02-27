2e87463bd17e2ecbe507374f1d32bde926c6d5d2
Laurent Thévenoux <thevenoux@adacore.com>
2024-02-27 11:56:46 +0100

    Fix user-defined literals aspect association name resolution

--- testsuite/tests/name_resolution/user_defined_str_lit/test.adb
+++ testsuite/tests/name_resolution/user_defined_str_lit/test.adb
@@ -2,9 +2,11 @@ procedure Test is
    package Pkg is
       type Str is null record
          with String_Literal => To_Str;
+      pragma Test_Block;
 
       function To_Str (X : Wide_Wide_String) return Str is
         (null record);
+      function To_Str (X : String) return Str is (null record);
    end Pkg;
 
    X : Pkg.Str := "Helloooo";

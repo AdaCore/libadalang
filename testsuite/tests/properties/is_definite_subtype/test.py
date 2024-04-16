25115122fe88806451c9886cf53ed89ab053ce1e
Raphaël AMIARD <amiard@adacore.com>
2024-04-16 11:03:25 +0200

    Extend is_definite_subtype to work on type expressions

--- testsuite/tests/properties/is_definite_subtype/test.py
+++ testsuite/tests/properties/is_definite_subtype/test.py
@@ -14,6 +14,13 @@ for filename in sys.argv[1:]:
         else:
             print('{} is not a definite subtype'.format(n))
 
+    for n in u.root.findall(lal.ObjectDecl):
+        te = n.f_type_expr
+        if te.p_is_definite_subtype:
+            print('{} is a definite subtype'.format(te))
+        else:
+            print('{} is not a definite subtype'.format(te))
+
     print('')
 
 print('Done')

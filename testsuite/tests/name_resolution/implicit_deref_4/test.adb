feba4eab8b8d4a53a1ddb39d20875d7bda8c53fd
Laurent Thévenoux <thevenoux@adacore.com>
2024-08-08 14:35:26 +0200

    Improve Implicit_Dereference aspect name resolution

--- testsuite/tests/name_resolution/implicit_deref_4/test.adb
+++ testsuite/tests/name_resolution/implicit_deref_4/test.adb
@@ -1,8 +1,9 @@
 procedure Test is
    type Database is tagged null record;
 
-   type Connection (D : access Database) is tagged null record
+   type Connection (C, D : access Database) is null record
      with Implicit_Dereference => D;
+   pragma Test_Block;
 
    subtype Sub_Connection is Connection;
 
@@ -14,6 +15,12 @@ procedure Test is
    begin
       null;
    end P;
+
+   D : access Database;
+
+   type Sub_Connection_3 is new Connection
+     with Implicit_Dereference => D;
+   pragma Test_Block;
 begin
    null;
 end;

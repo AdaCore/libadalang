90a45ad1b10fa33ee53fc699d8f33135e7029344
Romain Beguet <beguet@adacore.com>
2024-08-23 17:10:45 +0200

    Create proper synthesized node hierarchies.

---
+++ testsuite/tests/navigation/parent_of_synthetic_attr_subp/test.adb
@@ -0,0 +1,10 @@
+procedure Test is
+   V : Integer := 42;
+   X : String := V'Image;
+   --% node.f_default_expr.p_called_subp_spec.parent
+   --% node.f_default_expr.p_called_subp_spec.parent.parent
+begin
+   null;
+end Test;
+
+

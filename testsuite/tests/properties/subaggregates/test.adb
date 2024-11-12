00fd19a4e827e03a6d7b200e0c15762345579ca5
Romain Beguet <beguet@adacore.com>
2024-11-12 12:28:43 +0100

    Add utility properties for subaggregates.

--- testsuite/tests/properties/subaggregates/test.adb
+++ testsuite/tests/properties/subaggregates/test.adb
@@ -3,10 +3,17 @@ procedure Test is
       (('g', 'h'), (others => 'y'));
    --% root_aggr = node.f_default_expr
    --% root_aggr.p_is_subaggregate
+   --% root_aggr.p_subaggregate_array_type
+
    --% sub_aggr_1 = root_aggr.f_assocs[0].f_r_expr
    --% sub_aggr_1.p_is_subaggregate
+   --% sub_aggr_1.p_subaggregate_array_type
+   --% sub_aggr_1.p_subaggregate_dimension
+
    --% sub_aggr_2 = root_aggr.f_assocs[1].f_r_expr
    --% sub_aggr_2.p_is_subaggregate
+   --% sub_aggr_2.p_subaggregate_array_type
+   --% sub_aggr_2.p_subaggregate_dimension
 begin
    null;
 end Test;

8ebce96c7acf282b4743d0d368731a6422bad862
Pierre-Marie de Rodat <derodat@adacore.com>
2025-05-23 13:22:17 +0000

    Libadalang.Project_Provider.Default_Filename_For_Unit: fix crashes
    Recent work that introduced `Default_Filename_For_Unit` was incomplete:
    when the root project used in a project provider cannot have sources
    (abstract projects, aggregate projects, etc.), the attempt to call
    GPR2's `Filename_For_Unit` crashed. Add protections to avoid this
    situations.

--- testsuite/tests/ada_api/gpr2_missing_file/main.adb
+++ testsuite/tests/ada_api/gpr2_missing_file/main.adb
@@ -9,46 +9,61 @@ with Libadalang.Project_Provider; use Libadalang.Project_Provider;
 with Support;
 
 procedure Main is
-   Opts : GPR2.Options.Object;
-   Tree : GPR2.Project.Tree.Object;
-   Ctx  : Analysis_Context;
-   U    : Analysis_Unit;
-   D    : Basic_Decl;
-   B    : Body_Node;
+   procedure Check (Project_File : String);
+
+   -----------
+   -- Check --
+   -----------
+
+   procedure Check (Project_File : String) is
+      Opts : GPR2.Options.Object;
+      Tree : GPR2.Project.Tree.Object;
+      Ctx  : Analysis_Context;
+      U    : Analysis_Unit;
+      D    : Basic_Decl;
+      B    : Body_Node;
+   begin
+      Put_Line ("# " & Project_File);
+      New_Line;
+      Opts.Add_Switch (GPR2.Options.P, Project_File);
+      if not Tree.Load
+        (Options              => Opts,
+         With_Runtime         => True,
+         Artifacts_Info_Level => GPR2.Sources_Units)
+      then
+         raise Program_Error;
+      end if;
+
+      Ctx :=
+        Create_Context
+          (Unit_Provider => Create_Project_Unit_Provider (Tree),
+           Event_Handler => Support.Create_Event_Handler);
+      U := Ctx.Get_From_File ("pkg.1.ada");
+      if U.Has_Diagnostics then
+         for D of U.Diagnostics loop
+            Put_Line (U.Format_GNU_Diagnostic (D));
+         end loop;
+         raise Program_Error;
+      end if;
+
+      D :=
+        U.Root
+        .As_Compilation_Unit.F_Body
+        .As_Library_Item.F_Item
+        .As_Package_Decl.F_Public_Part
+        .F_Decls
+        .Child (1)
+        .As_Basic_Decl;
+      Put_Line ("Fetching the body of " & D.Image);
+      B := D.P_Body_Part_For_Decl;
+      Put_Line ("  -> " & B.Image);
+
+      New_Line;
+   end Check;
+
 begin
-   Opts.Add_Switch (GPR2.Options.P, "p.gpr");
-   if not Tree.Load
-     (Options              => Opts,
-      With_Runtime         => True,
-      Artifacts_Info_Level => GPR2.Sources_Units)
-   then
-      raise Program_Error;
-   end if;
+   Check ("p.gpr");
+   Check ("abstract_prj.gpr");
 
-   Ctx :=
-     Create_Context
-       (Unit_Provider => Create_Project_Unit_Provider (Tree),
-        Event_Handler => Support.Create_Event_Handler);
-   U := Ctx.Get_From_File ("pkg.1.ada");
-   if U.Has_Diagnostics then
-      for D of U.Diagnostics loop
-         Put_Line (U.Format_GNU_Diagnostic (D));
-      end loop;
-      raise Program_Error;
-   end if;
-
-   D :=
-     U.Root
-     .As_Compilation_Unit.F_Body
-     .As_Library_Item.F_Item
-     .As_Package_Decl.F_Public_Part
-     .F_Decls
-     .Child (1)
-     .As_Basic_Decl;
-   Put_Line ("Fetching the body of " & D.Image);
-   B := D.P_Body_Part_For_Decl;
-   Put_Line ("  -> " & B.Image);
-
-   New_Line;
    Put_Line ("main.adb: Done");
 end Main;

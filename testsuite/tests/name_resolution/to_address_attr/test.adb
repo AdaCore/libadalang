0dc5ed26c99519c793b28327a5bb86ee2667cda4
Romain Beguet <beguet@adacore.com>
2025-05-23 14:24:09 +0200

    Allow any integer type in the To_Address attribute.

--- testsuite/tests/name_resolution/to_address_attr/test.adb
+++ testsuite/tests/name_resolution/to_address_attr/test.adb
@@ -1,7 +1,15 @@
-with System;
+with System.Storage_Elements;
 
 procedure Test is
    X : System.Address := System'To_Address (0);
+   pragma Test_Statement_UID;
+
+   I : Integer := 12;
+   Y : System.Address := System'To_Address (I);
+   pragma Test_Statement_UID;
+
+   J : System.Storage_Elements.Integer_Address := 42;
+   Z : System.Address := System'To_Address (J);
    pragma Test_Statement_UID;
 begin
    null;

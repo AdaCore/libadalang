37c02dbb14408a67c795adb2f8d071d5a0eb2811
Romain Beguet <beguet@adacore.com>
2025-08-05 15:17:38 +0200

    Handle attribute subprograms of instantiated types.

--- testsuite/tests/name_resolution/generic_formal_base_type/test.adb
+++ testsuite/tests/name_resolution/generic_formal_base_type/test.adb
@@ -5,10 +5,25 @@ procedure Test is
       X : T'Base := T'First;
 
       Y : T := X * X;
+
+      subtype U is T;
+
+      type V is new Integer;
    end Pkg;
 
    package My_Pkg is new Pkg (Positive);
    pragma Test_Statement;
 begin
    My_Pkg.X := 2;
+
+   My_Pkg.X := My_Pkg.U'Succ (My_Pkg.X);
+   pragma Test_Statement;
+
+   declare
+      subtype V is My_Pkg.V;
+      Y : V := V'Succ (3);
+      pragma Test_Statement;
+   begin
+      null;
+   end;
 end Test;

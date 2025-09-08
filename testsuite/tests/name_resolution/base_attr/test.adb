d49033e33918e96be40adfe1b8d787f3f0c0d312
Romain Beguet <beguet@adacore.com>
2025-09-08 15:30:43 +0200

    Fix resolution of `'Base` attribute on integer types.

--- testsuite/tests/name_resolution/base_attr/test.adb
+++ testsuite/tests/name_resolution/base_attr/test.adb
@@ -7,6 +7,46 @@ procedure Test is
 
    Z : Integer := T'Base'Alignment;
    pragma Test_Statement;
+
+   type I_T is range 0 .. 10;
+   type I_D_T is new I_T range 0 .. 5;
+
+   X_I : I_T'Base := 10;
+   pragma Test_Statement;
+   X_I_D : I_D_T'Base := 10;
+   pragma Test_Statement;
+
+   type E_T is (A, B, C);
+   type E_D_T is new E_T range A .. B;
+
+   X_E : E_T'Base := C;
+   pragma Test_Statement;
+   X_E_D : E_D_T'Base := C;
+   pragma Test_Statement;
+
+   type M_T is mod 10;
+   type M_D_T is new M_T range 0 .. 5;
+
+   X_M : M_T'Base := 9;
+   pragma Test_Statement;
+   X_M_D : M_D_T'Base := 9;
+   pragma Test_Statement;
+
+   type FL_T is digits 10;
+   type FL_D_T is new FL_T range 0.0 .. 100.0;
+
+   X_FL : FL_T'Base := 1000.0;
+   pragma Test_Statement;
+   X_FL_D : FL_D_T'Base := 1000.0;
+   pragma Test_Statement;
+
+   type FP_T is delta 0.1 range 0.0 .. 10.0;
+   type FP_D_T is new FP_T range 0.0 .. 5.0;
+
+   X_FP : FP_T'Base := 10.0;
+   pragma Test_Statement;
+   X_FP_D : FP_D_T'Base := 10.0;
+   pragma Test_Statement;
 begin
    null;
 end Test;

a85b984040dd80713c34766a353e02274e31efea
Pierre-Marie de Rodat <derodat@adacore.com>
2025-09-30 13:39:43 +0000

    c_api/gpr_config_file: fix a memory leak
    Free the ada_gpr_options data structure even on error.

--- testsuite/tests/c_api/gpr_config_file/main.c
+++ testsuite/tests/c_api/gpr_config_file/main.c
@@ -12,6 +12,7 @@ main (void)
     ada_gpr_options opts;
     ada_gpr_project gpr;
     ada_string_array_ptr errors;
+    int had_error;
 
     opts = ada_gpr_options_create ();
     abort_on_exception ();
@@ -23,25 +24,27 @@ main (void)
     abort_on_exception ();
 
     ada_gpr_project_load (opts, 0, &gpr, &errors);
-    if (print_exception (false))
-      return 0;
-
-    /* This is supposed to be dead code (the project loading is supposed to
-       abort with a fatal error), but just in case it does not, print the
-       errors we get.  */
-    if (errors->length > 0)
+    had_error = print_exception (false);
+    if (! had_error)
       {
-        printf ("Errors:\n");
-        for (int i = 0 ; i < errors->length; i++)
+	/* This is supposed to be dead code (the project loading is supposed to
+	   abort with a fatal error), but just in case it does not, print the
+	   errors we get.  */
+	if (errors->length > 0)
 	  {
-	    printf ("   - %s\n", errors->c_ptr[i]);
+	    printf ("Errors:\n");
+	    for (int i = 0 ; i < errors->length; i++)
+	      {
+		printf ("   - %s\n", errors->c_ptr[i]);
+	      }
 	  }
+
+	ada_gpr_project_free (gpr);
+	abort_on_exception ();
+	ada_free_string_array (errors);
+	abort_on_exception ();
       }
 
-    ada_gpr_project_free (gpr);
-    abort_on_exception ();
-    ada_free_string_array (errors);
-    abort_on_exception ();
     ada_gpr_options_free (opts);
     abort_on_exception ();
     return 0;

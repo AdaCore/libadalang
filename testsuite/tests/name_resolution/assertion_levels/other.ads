894803c72626fc990490fa51ab204eec1ea27e55
Romain Beguet <beguet@adacore.com>
2026-02-17 14:57:40 +0100

    Extend test with assertion levels defined in external config pragmas.

--- testsuite/tests/name_resolution/assertion_levels/other.ads
+++ testsuite/tests/name_resolution/assertion_levels/other.ads
@@ -4,7 +4,12 @@ package Other is
    procedure Foo
       with Post => (L1 => True, L2 => True);
    pragma Test_Block;
+
+   procedure Bar
+      with Post => (L3 => True);
+   pragma Test_Block;
 private
 
    procedure Foo is null;
+   procedure Bar is null;
 end Other;

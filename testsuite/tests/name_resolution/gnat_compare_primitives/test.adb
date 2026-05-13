1b2c550f4540a9a283923f9305d2de02b2fff419
Romain Beguet <beguet@adacore.com>
2026-05-13 17:24:26 +0200

    gnat_compare: properly handle tagged types.

--- testsuite/tests/name_resolution/gnat_compare_primitives/test.adb
+++ testsuite/tests/name_resolution/gnat_compare_primitives/test.adb
@@ -11,11 +11,26 @@ procedure Test is
       function Baz return T;
 
       function Bazz return Integer;
+
+      procedure Also (X : I; Y : T) is null;
+
+      type Priv is private;
+      type Priv_Tagged is tagged private;
+
+      function Foo_Bar (X : I; Y : Priv_Tagged) return Integer;
+
+   private
+
+      type Priv is tagged null record;
+      type Priv_Tagged is tagged null record;
+
+      function Foo_Baz (X : I; Y : Priv) return Integer is (0);
    end Pkg;
 
    package body Pkg is
       function Baz return T is (null record);
       function Bazz return Integer is (0);
+      function Foo_Bar (X : I; Y : Priv_Tagged) return Integer is (0);
    end Pkg;
 begin
    null;

f93e455f6e9b9b44460923d025539ab49f3a5fa9
Laurent Thévenoux <thevenoux@adacore.com>
2026-05-19 14:40:02 +0200

    Support dotted names for `pragma Warnings` local_name argument

--- testsuite/tests/name_resolution/pragma_warnings/test.adb
+++ testsuite/tests/name_resolution/pragma_warnings/test.adb
@@ -17,6 +17,12 @@ procedure Test is
 
    procedure P is null;
    function F (I : Integer) return Boolean is (True);
+
+   package Inner is
+      I : Integer;
+   end Inner;
+   pragma Warnings (Off, Inner.I);
+   pragma Test_Statement;
 begin
    null;
 end Test;

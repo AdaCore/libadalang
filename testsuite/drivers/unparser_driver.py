ddbb51300a967c0abb91c309e1ba07e56f7688f1
Pierre-Marie de Rodat <derodat@adacore.com>
2026-06-04 09:29:57 +0000

    Testsuite: make unparsing runtime errors visible

--- testsuite/drivers/unparser_driver.py
+++ testsuite/drivers/unparser_driver.py
@@ -39,24 +39,31 @@ class UnparserDriver(BaseDriver):
         ]
 
     def run(self) -> None:
-        # Run the unparser on "input.ada" for the given grammar rule and
+        # Run the unparser on "input.ada" for the given grammar rule.
         # unparse it with the default unparsing configuration.
-        #
-        # Also run lal_unparse's auto-checks and check that all nodes have an
-        # explicit configuration.
-        argv = ["lal_unparse", "-A", "-C", "-w", "79", "-i", "3", "-I", "2"]
-
+        argv = ["lal_unparse", self.input_filename]
         rule = self.test_env.get("rule")
         if rule:
             argv += ["-r", rule]
 
         # Even though we are using the default unparsing configuration, pass
         # the JSON file explicitly so that one does not need to rebuild
-        # Liblktlang in order to test a change in that configuration.
-        argv += ["-c", self.unparsing_config_filename, self.input_filename]
+        # Libadalang in order to test a change in that configuration.
+        argv += ["-c", self.unparsing_config_filename]
 
-        # Likewise for overridings
+        # Pass the requested overriding files
         for filename in self.unparsing_config_overridings:
             argv += ["--overriding", filename]
+
+        # Target 79 columns lines, 3 spaces for regular indentation, 2 spaces
+        # for continuation indentation.
+        argv += ["-w", "79", "-i", "3", "-I", "2"]
+
+        # Enable unparsing auto-checks and check that all nodes have an
+        # explicit configuration.
+        argv += ["-A", "-C"]
+
+        # Make runtime exceptions visible
+        argv += ["-t", "expansion_errors"]
 
         self.run_and_check(argv, memcheck=True)

606cc85db6b4fde7a9ee994911b9ac18f1bceeb0
Romain Beguet <beguet@adacore.com>
2026-07-21 15:17:51 +0200

    Op node should not be a token node.

--- testsuite/tests/properties/canonical_text/foo.ads
+++ testsuite/tests/properties/canonical_text/foo.ads
@@ -16,6 +16,15 @@ procedure Foo is
       'a'
       --% node.p_canonical_text
    );
+
+   O_1 : constant Integer := 1 + 1;
+   --% node.f_default_expr.f_op.p_canonical_text
+
+   O_2 : constant Boolean := True and then False;
+   --% node.f_default_expr.f_op.p_canonical_text
+
+   O_3 : constant Boolean := 1 not in 1 | 2;
+   --% node.f_default_expr.f_op.p_canonical_text
 begin
    null;
 end Foo;
